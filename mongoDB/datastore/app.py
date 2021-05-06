82a35d78816f22b9e47a8345c5b57c35626b64ae
Vishal Mahuli <mahulivishal@Vishals-MacBook-Pro.local>
2021-05-06 16:31:24 +0530

    changed port

--- mongoDB/datastore/app.py
+++ mongoDB/datastore/app.py
@@ -6,4 +6,4 @@ from configs import set_cluster
 if __name__ == '__main__':
     datastore = Datastore()
     datastore.set_mongo_cluster(set_cluster)
-    ulcdatastoreapp.run(host="localhost", port=5010, threaded=True)
+    ulcdatastoreapp.run(host="52.39.16.42", port=5010, threaded=True)
