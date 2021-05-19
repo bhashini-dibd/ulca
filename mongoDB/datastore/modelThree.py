bc21289227edbbe47eb8e3fd7e2af75c26fb70a0
Vishal Mahuli <mahulivishal@Vishals-MacBook-Pro.local>
2021-05-19 16:50:25 +0530

    rm batch proc

--- mongoDB/datastore/modelThree.py
+++ mongoDB/datastore/modelThree.py
@@ -10,10 +10,8 @@ from datetime import datetime
 from functools import partial
 from logging.config import dictConfig
 
-from bson import Code
-
 from configs import file_path, file_name, default_offset, default_limit, mongo_server_host, mongo_ulca_m3_db
-from configs import mongo_ulca_dataset_m3_col, no_of_enrich_process, no_of_dump_process
+from configs import mongo_ulca_dataset_m3_col, no_of_dump_process
 
 import pymongo
 log = logging.getLogger('file')
@@ -38,48 +36,21 @@ class ModelThree:
             if 'slice' in request.keys():
                 data_json = data_json[request["slice"]["start"]:request["slice"]["end"]]
             total, duplicates, batch = len(data_json), 0, request["batch"]
-            update_batch, update_records, insert_batch, insert_records = [], [], [], []
-            log.info(f'Enriching the dataset..... | {datetime.now()}')
-            func = partial(self.get_enriched_data, request=request)
-            pool_enrichers = multiprocessing.Pool(no_of_enrich_process)
+            log.info(f'Enriching and dumping the dataset..... | {datetime.now()}')
+            func = partial(self.enriched_and_persist, request=request)
+            pool_enrichers = multiprocessing.Pool(no_of_dump_process)
             enrichment_processors = pool_enrichers.map_async(func, data_json).get()
+            u_count, i_count, = 0, 0
             for result in enrichment_processors:
                 if result:
-                    if result[1]:
-                        if "INSERT" == result[1]:
-                            if len(insert_batch) == batch:
-                                log.info(f'Adding batch of {len(insert_batch)} to the BULK INSERT list... | {datetime.now()}')
-                                insert_records.append(insert_batch)
-                                insert_batch = []
-                            insert_batch.append(result[0])
-                        if "UPDATE" == result[1]:
-                            if len(update_batch) == batch:
-                                log.info(f'Adding batch of {len(update_batch)} to the BULK UPDATE list... | {datetime.now()}')
-                                update_records.append(update_batch)
-                                update_batch = []
-                            update_batch.append(result[0])
-                    duplicates += result[2]
+                    if result[0]:
+                        if "INSERT" == result[0]:
+                            i_count += 1
+                        if "UPDATE" == result[0]:
+                            u_count += 1
+                    duplicates += result[1]
             pool_enrichers.close()
-            if insert_batch:
-                log.info(f'Adding batch of {len(insert_batch)} to the BULK INSERT list... | {datetime.now()}')
-                insert_records.append(insert_batch)
-            if update_batch:
-                log.info(f'Adding batch of {len(update_batch)} to the BULK UPDATE list... | {datetime.now()}')
-                update_records.append(update_batch)
-            pool_ins, pool_upd, ins_count, upd_count = multiprocessing.Pool(no_of_dump_process), multiprocessing.Pool(no_of_dump_process), 0, 0
-            if update_records:
-                log.info(f'Dumping UPDATE records.... | {datetime.now()}')
-                processors = pool_upd.map_async(self.update, update_records).get()
-                for result in processors:
-                    upd_count += result
-            pool_upd.close()
-            if insert_records:
-                log.info(f'Dumping INSERT records.... | {datetime.now()}')
-                processors = pool_ins.map_async(self.insert, insert_records).get()
-                for result in processors:
-                    ins_count += result
-            pool_ins.close()
-            log.info(f'Done! -- UPDATES: {upd_count}, INSERTS: {ins_count}, "DUPLICATES": {duplicates} | {datetime.now()}')
+            log.info(f'Done! -- UPDATES: {u_count}, INSERTS: {i_count}, "DUPLICATES": {duplicates} | {datetime.now()}')
         except Exception as e:
             log.exception(e)
             return {"message": "EXCEPTION while loading dataset!!", "status": "FAILED"}
@@ -95,7 +66,7 @@ class ModelThree:
             else:
                 yield v
 
-    def get_enriched_data(self, data, request):
+    def enriched_and_persist(self, data, request):
         if 'sourceText' not in data.keys() or 'targetText' not in data.keys():
             return None, None, 0
         append_record = None
@@ -105,7 +76,7 @@ class ModelThree:
         if record:
             record = record[0]
             if src_hash in record["tags"] and tgt_hash in record["tags"]:
-                return None, None, 1
+                return None, 1
             else:
                 append_record = record
         target = {
@@ -132,7 +103,8 @@ class ModelThree:
             for target in append_record["targets"]:
                 langs.append(target["targetLanguage"])
             append_record["shardKey"] = ','.join(map(str, sorted(langs)))
-            return append_record, "UPDATE", 0
+            self.update(append_record)
+            return "UPDATE", 0
         else:
             tags_dict = {
                 "srcHash": src_hash, "tgtHash": tgt_hash, "tgtLang": request["details"]["targetLanguage"],
@@ -148,7 +120,8 @@ class ModelThree:
                 "submitter": request["submitter"], "contributors": request["contributors"],
                 "targets": [target], "tags": tags, "shardKey": shard_key
             }
-            return record, "INSERT", 0
+            self.insert(record)
+            return "INSERT", 0
 
     def get_dataset_internal(self, query):
         try:
@@ -174,9 +147,9 @@ class ModelThree:
             if 'maxScore' in query.keys():
                 score_query["$lte"] = query["maxScore"]
             if score_query:
-                db_query["scoreQuery"] = {"data.score": score_query}
+                db_query["scoreQuery"] = {"targets.alignmentScore": score_query}
             if 'score' in query.keys():
-                db_query["scoreQuery"] = {"targets.score": query["score"]}
+                db_query["scoreQuery"] = {"targets.alignmentScore": query["score"]}
             tags = []
             if 'srcLang' in query.keys():
                 db_query["srcLang"] = query["srcLang"]
@@ -247,16 +220,15 @@ class ModelThree:
 
     def insert(self, data):
         col = self.get_mongo_instance()
-        col.insert_many(data)
-        return len(data)
+        col.insert(data)
+        return 1
 
     def update(self, data):
         col = self.get_mongo_instance()
         bulk = col.initialize_unordered_bulk_op()
-        for record in data:
-            bulk.find({'id': record["id"]}).update({'$set': {'targets': record["targets"], "tags": record["tags"]}})
+        bulk.find({'id': data["id"]}).update({'$set': {'targets': data["targets"], "tags": data["tags"], "shardKey": data["shardKey"]}})
         bulk.execute()
-        return len(data)
+        return 1
 
     # Searches the object into mongo collection
     def search(self, query, internal):
