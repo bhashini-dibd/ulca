fa92f99a7bda86b688dd4599e2643a3598a737ed
Vishal Mahuli <mahulivishal@Vishals-MacBook-Pro.local>
2021-06-03 14:08:39 +0530

    del venv

--- backend/dataset/publish/venv/lib/python3.8/site-packages/transliterate/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-from transliterate.tests.test_transliterate import TransliterateTest
-
-if __name__ == '__main__':
-    unittest.main()
--- backend/dataset/publish/venv/lib/python3.8/site-packages/transliterate/tests/data/__init__.py
+++
@@ -1,6 +0,0 @@
-from .default import *
-
-__title__ = 'transliterate.tests.data'
-__author__ = 'Artur Barseghyan'
-__copyright__ = '2013-2018 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
