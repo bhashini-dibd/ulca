e8f6435ff2c53e655683b920afcea80800368440
Vishal Mahuli <mahulivishal@Vishals-MacBook-Pro.local>
2021-06-07 12:35:51 +0530

    prc tracker and events

---
+++ backend/dataset/publish/events/metrics.py
@@ -0,0 +1,48 @@
+import logging
+from logging.config import dictConfig
+
+log = logging.getLogger('file')
+
+
+mongo_instance = None
+
+class MetricEvent:
+    def __init__(self):
+        pass
+
+    def create_metric_event(self, data):
+        pass
+
+
+# Log config
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] {%(filename)s:%(lineno)d} %(threadName)s %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {
+        'info': {
+            'class': 'logging.FileHandler',
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'filename': 'info.log'
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'stream': 'ext://sys.stdout',
+        }
+    },
+    'loggers': {
+        'file': {
+            'level': 'DEBUG',
+            'handlers': ['info', 'console'],
+            'propagate': ''
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['info', 'console']
+    }
+})
---
+++ backend/dataset/publish/processtracker/processtracker.py
@@ -0,0 +1,56 @@
+import logging
+from logging.config import dictConfig
+
+log = logging.getLogger('file')
+
+
+mongo_instance = None
+
+class ProcessTracker:
+    def __init__(self):
+        pass
+
+    def create_task_event(self, data):
+        pass
+
+    def update_task_event(self, data):
+        pass
+
+    def search_task_event(self, data):
+        pass
+
+
+
+
+# Log config
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] {%(filename)s:%(lineno)d} %(threadName)s %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {
+        'info': {
+            'class': 'logging.FileHandler',
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'filename': 'info.log'
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'stream': 'ext://sys.stdout',
+        }
+    },
+    'loggers': {
+        'file': {
+            'level': 'DEBUG',
+            'handlers': ['info', 'console'],
+            'propagate': ''
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['info', 'console']
+    }
+})
