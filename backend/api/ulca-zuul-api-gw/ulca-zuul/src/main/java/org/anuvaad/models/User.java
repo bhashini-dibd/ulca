bc7912d38f0d30e36c9b3c078089712f7cbee8df
Vishal Mahuli <mahulivishal@Vishals-MacBook-Pro.local>
2021-06-08 18:08:53 +0530

    zuul gateway

---
+++ backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/anuvaad/models/User.java
@@ -0,0 +1,109 @@
+package org.anuvaad.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+
+public class User {
+
+    @JsonProperty("userID")
+    public String userID;
+
+    @JsonProperty("name")
+    public String name;
+
+    @JsonProperty("userName")
+    public String userName;
+
+    @JsonProperty("email")
+    public String email;
+
+    @JsonProperty("phoneNo")
+    public String phoneNo;
+
+    @JsonProperty("orgID")
+    public String orgID;
+
+    @JsonProperty("roles")
+    public List<UserRole> roles;
+
+    @JsonProperty("publicKey")
+    public String publicKey;
+
+    @JsonProperty("privateKey")
+    public String privateKey;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setOrgID(String orgID) {
+        this.orgID = orgID;
+    }
+
+    public String getOrgID() {
+        return orgID;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public List<UserRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<UserRole> roles) {
+        this.roles = roles;
+    }
+}
+
