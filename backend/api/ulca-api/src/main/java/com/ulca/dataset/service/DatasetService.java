f41e152fe9d960a25b1ea678ce5c97857e0daf95
sarojsingh2021 <saroj.kumarsingh@tarento.com>
2021-06-10 12:44:20 +0530

    dataset getByUserId api

--- backend/api/ulca-api/src/main/java/com/ulca/dataset/service/DatasetService.java
+++ backend/api/ulca-api/src/main/java/com/ulca/dataset/service/DatasetService.java
@@ -1,6 +1,9 @@
 package com.ulca.dataset.service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +19,16 @@ import com.ulca.dataset.kakfa.FileDownload;
 import com.ulca.dataset.model.Dataset;
 import com.ulca.dataset.model.Fileidentifier;
 import com.ulca.dataset.model.ProcessTracker;
+import com.ulca.dataset.model.ProcessTracker.ServiceRequestActionEnum;
+import com.ulca.dataset.model.ProcessTracker.ServiceRequestTypeEnum;
+import com.ulca.dataset.model.ProcessTracker.StatusEnum;
 import com.ulca.dataset.request.DatasetSubmitRequest;
 import com.ulca.dataset.response.DatasetSubmitResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+import com.ulca.dataset.response.DatasetListByUserIdResponse;
+@Slf4j
 @Service
 public class DatasetService {
 	
@@ -39,13 +49,13 @@ public class DatasetService {
     private String fileDownloadTopic;
 	
 	@Transactional
-	public DatasetSubmitResponse datasetSubmit(DatasetSubmitRequest request) {
-		
+	public DatasetSubmitResponse datasetSubmit(DatasetSubmitRequest request,String userId ) {
 		
 		Dataset dataset = new Dataset();
 		dataset.setDatasetName(request.getDatasetName());
 		dataset.setDatasetType(request.getType());
 		dataset.setCreatedOn(new Date());
+	
 		
 		Fileidentifier fileIndetifier = new Fileidentifier();
 		fileIndetifier.setFileLocationURL(request.getUrl());
@@ -59,10 +69,15 @@ public class DatasetService {
 		datasetDao.insert(dataset);
 		
 		ProcessTracker processTracker = new ProcessTracker();
-		processTracker.setUserId("6491af71d71b4f1d9cff293522260838");
+		processTracker.setUserId(userId);
 		processTracker.setDatasetId(dataset.getDatasetId());
 		UUID uuid = UUID.randomUUID();
 		processTracker.setServiceRequestNumber(uuid.toString());
+		processTracker.setServiceRequestAction(ServiceRequestActionEnum.SUBMIT);
+		processTracker.setServiceRequestType(ServiceRequestTypeEnum.DATATSET);
+		processTracker.setStatus(StatusEnum.NOTSTARTED);
+		processTracker.setStartTime(new Date());
+	
 		processTrackerDao.insert(processTracker);
 		
 		
@@ -75,6 +90,29 @@ public class DatasetService {
 		template.send(fileDownloadTopic, fileDownload);
 		
 		return new DatasetSubmitResponse(processTracker.getServiceRequestNumber(), dataset.getDatasetId(), dataset.getCreatedOn());
+	}
+	
+	public List<DatasetListByUserIdResponse> dataSetListByUserId(String userId) {
+		
+		log.info("******** Entry DatasetService:: dataSetListByUserId *******" );
+		
+		List<DatasetListByUserIdResponse> list = new ArrayList<DatasetListByUserIdResponse> ();
+		
+		
+		List<ProcessTracker>  processList = processTrackerDao.findByUserId(userId);
+		for(ProcessTracker p : processList) {
+			
+			Optional<Dataset> dataset = datasetDao.findById(p.getDatasetId());
+			
+			list.add(new DatasetListByUserIdResponse(p.getServiceRequestNumber(),dataset.get().getDatasetName(),dataset.get().getCreatedOn(),p.getStatus().toString()));	
+		}
+			
+			
+			
+		log.info("******** Exit DatasetService:: dataSetListByUserId *******" );
+		return list;
+		
+		
 	}
 
 }
