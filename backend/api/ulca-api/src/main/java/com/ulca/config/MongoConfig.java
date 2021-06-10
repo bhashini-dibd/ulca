a69643d5357fa063536cd4e8fa15af5799f4295c
sarojsingh2021 <saroj.kumarsingh@tarento.com>
2021-06-10 18:29:26 +0530

    conf change
    conf change

--- backend/api/ulca-api/src/main/java/com/ulca/config/MongoConfig.java
+++ backend/api/ulca-api/src/main/java/com/ulca/config/MongoConfig.java
@@ -1,32 +1,32 @@
-package com.ulca.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-
-@Configuration
-public class MongoConfig {
-	
-	
-
-	@Value(value = "${ULCA_MONGO_CLUSTER}")
-    private String mongodbUri;
-	
-	
-	@Value(value = "${ULCA_PROC_TRACKER_DB}")
-    private String database;
-
-	 public @Bean MongoClient mongoClient() {
-	       return MongoClients.create(mongodbUri);
-	   }
-
-	 public @Bean MongoDatabaseFactory mongoDatabaseFactory() {
-		    return new SimpleMongoClientDatabaseFactory(MongoClients.create(), database);
-		  }
-
-}
+//package com.ulca.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.MongoDatabaseFactory;
+//import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+//
+//import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoClients;
+//
+//@Configuration
+//public class MongoConfig {
+//	
+//	
+//
+//	@Value(value = "${ULCA_MONGO_CLUSTER}")
+//    private String mongodbUri;
+//	
+//	
+//	@Value(value = "${ULCA_PROC_TRACKER_DB}")
+//    private String database;
+//
+//	 public @Bean MongoClient mongoClient() {
+//	       return MongoClients.create(mongodbUri);
+//	   }
+//
+//	 public @Bean MongoDatabaseFactory mongoDatabaseFactory() {
+//		    return new SimpleMongoClientDatabaseFactory(MongoClients.create(), database);
+//		  }
+//
+//}
