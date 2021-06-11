b039ffd03e6c37bc060e570193b280c3ca449f1a
Jainy Joy <52598380+JainyJoy@users.noreply.github.com>
2021-06-11 19:05:37 +0530

    Update config.py

--- backend/metric/ulca-metric-api/config.py
+++ backend/metric/ulca-metric-api/config.py
@@ -10,8 +10,8 @@ download_folder = 'upload'
 ENABLE_CORS     = False
 
 # mongodb
-MONGO_DB_SCHEMA         = os.environ.get('MONGO_DB_SCHEMA', 'sangrah')
+MONGO_DB_SCHEMA         = os.environ.get('MONGO_DB_SCHEMA', 'test')
 MONGO_CONNECTION_URL    = os.environ.get('MONGO_CLUSTER_URL', 'mongodb://localhost:27017')
 
-DRUID_DB_SCHEMA         = os.environ.get('MATRIC_DRUID_DB_SCHEMA', 'TEST2')
+DRUID_DB_SCHEMA         = os.environ.get('MATRIC_DRUID_DB_SCHEMA', 'dataset-training-v3')
 DRUID_CONNECTION_URL    = os.environ.get('DRUID_CLUSTER_URL', 'druid://localhost:8082/druid/v2/sql/')
