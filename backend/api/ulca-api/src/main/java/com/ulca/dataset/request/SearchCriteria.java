e76d89e138adcfaaa65d31d824ff5ae5515d9a3b
sarojsingh2021 <saroj.kumarsingh@tarento.com>
2021-06-12 15:28:43 +0530

    search criteria ingore null properties
    search criteria ingore null properties

--- backend/api/ulca-api/src/main/java/com/ulca/dataset/request/SearchCriteria.java
+++ backend/api/ulca-api/src/main/java/com/ulca/dataset/request/SearchCriteria.java
@@ -1,5 +1,7 @@
 package com.ulca.dataset.request;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -11,6 +13,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchCriteria {
 	
 	private String licence;
