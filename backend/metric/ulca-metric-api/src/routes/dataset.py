f58740519037d3a25b8b9473d3e75ccfcd974414
JainyJoy <jainyjoy97@gmail.com>
2021-06-15 16:34:30 +0530

    updating contract

--- backend/metric/ulca-metric-api/src/routes/dataset.py
+++ backend/metric/ulca-metric-api/src/routes/dataset.py
@@ -1,13 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from src.resources.dataset import ParallelCorpusCreateResource, DatasetSearchResource
+from src.resources.dataset import  DatasetSearchResource
 
 CORPUS_BLUEPRINT = Blueprint("corpus", __name__)
 
-Api(CORPUS_BLUEPRINT).add_resource(
-    ParallelCorpusCreateResource, "/v0/dataset/parallel-corpus/submit"
-)
 
 Api(CORPUS_BLUEPRINT).add_resource(
     DatasetSearchResource, "/v0/store/search"
