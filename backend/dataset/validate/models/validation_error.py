91fdc484c4df58057e101cbc3c9ac41328e756a0
dheeraj gujral <dheerajgujral47@gmail.com>
2021-06-24 23:13:03 +0530

    Removing unused file

--- backend/dataset/validate/models/validation_error.py
+++
@@ -1,5 +0,0 @@
-import enum
-
-class ValidationError(enum.Enum):
-    ERR_SENTENCE_TOO_SHORT = 'The text is too short, min word count should be 4'
-    ERR_WORD_LENGTH_TOO_SHORT = 'The average word length for the sentence is too small'
