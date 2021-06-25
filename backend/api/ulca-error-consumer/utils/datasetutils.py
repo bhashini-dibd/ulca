dd3b12725d66d20fabc2fc792d3498ff88181435
Jainy Joy <52598380+JainyJoy@users.noreply.github.com>
2021-06-25 17:55:25 +0530

    Update datasetutils.py

--- backend/api/ulca-error-consumer/utils/datasetutils.py
+++ backend/api/ulca-error-consumer/utils/datasetutils.py
@@ -128,8 +128,6 @@ class DatasetUtils:
             headers =   {"Content-Type": "application/json"}
             body    =   {"fileName":file_name,"storageFolder":folder_name}
             request_url = file_store_host+file_store_upload_endpoint
-            body ={"fileName": "/opt/error-fac945a3-4fa0-49b1-9a2f-a61b72cd20b2.csv","storageFolder": "errors"}
-            request_url = "http://dev-auth.ulcacontrib.org/ulca/file-store/v0/file/upload"
             log.info("Intiating request to store data on object store %s"%request_url)
             response = requests.post(url=request_url, headers = headers, json = body)
             response_data = response.content
