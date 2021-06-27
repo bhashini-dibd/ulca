dd39f772a1850353b09ed92c91b223b94571d2e8
sajish <sajish.sasidharan@tarento.com>
2021-06-27 13:54:04 +0530

    added

--- frontend/webapp/src/ui/container/DataSet/DatasetView/DetailedStatus.jsx
+++ frontend/webapp/src/ui/container/DataSet/DatasetView/DetailedStatus.jsx
@@ -68,7 +68,7 @@ const DetailedStatus = (props) => {
                 return (
                         <div className={classes.headerButtons}>
                                 <Typography  variant="h5" >{name}</Typography>
-                                { <Button color={"primary" } size="medium" className = {classes.ButtonRefresh} variant="outlined" disabled={(status==="in-progress")? false:true}   onClick={() => DetailedDataSetStatusApi()}><Cached className ={classes.iconStyle}/>Refresh</Button>}
+                                { <Button color={"primary" } size="medium" className = {classes.ButtonRefresh} variant="outlined" disabled={(status==="In-Progress")? false:true}   onClick={() => DetailedDataSetStatusApi()}><Cached className ={classes.iconStyle}/>Refresh</Button>}
                                 <Button color={"primary"} href={errorData.file} target="_self" size="medium" variant="outlined" disabled={(errorData.hasOwnProperty("file"))? false:true} className={!(errorData.status!=="completed")? classes.ButtonRefresh : classes.buttonStyle } onClick={() => handleDownload()}><SaveAlt className ={classes.iconStyle}/>Error Logs</Button>
                         
                         </div>
