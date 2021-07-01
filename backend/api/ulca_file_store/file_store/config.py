5b2906769b100fd2b3d702fa4aeedb6049473afb
Jainy Joy <52598380+JainyJoy@users.noreply.github.com>
2021-07-01 18:29:26 +0530

    update aws link prefix

--- backend/api/ulca_file_store/file_store/config.py
+++ backend/api/ulca_file_store/file_store/config.py
@@ -15,7 +15,7 @@ aws_file_prefix         =   os.environ.get('ULCA_AWS_FILE_PREFIX',"errors/")
 aws_access_key          =   os.environ.get('ULCA_AWS_S3_ACCESS_KEY', 'access-key')
 aws_secret_key          =   os.environ.get('ULCA_AWS_S3_SECRET_KEY', 'secret-key')
 aws_bucket_name         =   os.environ.get('ULCA_AWS_BUCKET_NAME', 'ulca-datasets')
-aws_link_prefix         =   f'http://{aws_bucket_name}.s3.amazonaws.com/'
+aws_link_prefix         =   f'https://{aws_bucket_name}.s3.amazonaws.com/'
 
 azure_connection_string =   os.environ.get('ULCA_AZURE_CONNECTION_STRING',"connection_string")
 azure_container_name    =   os.environ.get('ULCA_AZURE_FILE_CONTAINER',"container_name")
