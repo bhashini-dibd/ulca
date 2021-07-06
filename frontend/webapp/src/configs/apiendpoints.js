9f16f23d7b942994a7991f3f982e7bc7830de697
sajish <sajish.sasidharan@tarento.com>
2021-07-06 18:18:05 +0530

    chnaged endpoints

--- frontend/webapp/src/configs/apiendpoints.js
+++ frontend/webapp/src/configs/apiendpoints.js
@@ -1,13 +1,13 @@
 const endpoints = {
 
-  getContributionList: '/dataset/listByUserId',
-  getDetailReport: '/dataset/getByServiceRequestNumber',
+  getContributionList: '/ulca/apis/v0/dataset/listByUserId',
+  getDetailReport: '/ulca/apis/v0/dataset/getByServiceRequestNumber',
   dataSetSearchApi: '/ulca/data-metric/v0/store/search',
   login: '/ulca/user-mgmt/v1/users/login',
-  datasetSubmit: '/dataset/corpus/submit',
+  datasetSubmit: '/ulca/apis/v0/dataset/corpus/submit',
   getSearchOptions: '/ulca/user-mgmt/v1/users/login',
-  mySearches: "/dataset/corpus/search/listByUserId",
-  submitSearchReq: '/dataset/corpus/search',
+  mySearches: "/ulca/apis/v0/dataset/corpus/search/listByUserId",
+  submitSearchReq: '/ulca/apis/v0/dataset/corpus/search',
   errorReport:'/ulca/error-consumer/v0/error/report',
   register:"/ulca/user-mgmt/v1/users/signup",
   activateUser :"/ulca/user-mgmt/v1/users/verify-user"
