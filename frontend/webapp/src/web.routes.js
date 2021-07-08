7b57854b2193d418932c9ff6a68d4cfdaae02617
sajish <sajish.sasidharan@tarento.com>
2021-07-08 11:53:37 +0530

    added configs

--- frontend/webapp/src/web.routes.js
+++ frontend/webapp/src/web.routes.js
@@ -6,14 +6,18 @@ import {
 import history from "./web.history";
 import Dashboard from "./components/Chart";
 export default function App() {
-  debugger
   return (
-    <Router history={history} basename="/intro">
+
+    
+    <Router history={history} basename="">
+      
       <div>
         <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`}
+          <Route exact path={`${process.env.PUBLIC_URL}`}
             component={Dashboard}
+            
           />
+
         </Switch>
       </div>
     </Router>
