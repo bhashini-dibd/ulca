d6089942a66d9b0b98a4985352fd844c683be2fc
sarojsingh2021 <saroj.kumarsingh@tarento.com>
2021-07-20 14:16:02 +0530

    remove cors filter
    remove cors filter

--- backend/api/ulca-api/src/main/java/com/ulca/dataset/controller/DatasetController.java
+++ backend/api/ulca-api/src/main/java/com/ulca/dataset/controller/DatasetController.java
@@ -30,7 +30,7 @@ import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value= "/ulca/apis/v0/dataset")
 public class DatasetController {
