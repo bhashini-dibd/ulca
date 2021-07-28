a258f062eaf812d125ddb7394e78751f692c8048
sajish <sajish.sasidharan@tarento.com>
2021-07-28 13:44:55 +0530

    added styles

--- frontend/webapp/src/ui/container/Model/ModelView/ContributionList.jsx
+++ frontend/webapp/src/ui/container/Model/ModelView/ContributionList.jsx
@@ -1,5 +1,6 @@
 import { withStyles, Button } from "@material-ui/core";
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from "../../../styles/Datatable"
 import BreadCrum from '../../../components/common/Breadcrum';
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -38,169 +39,6 @@ const ContributionList = (props) => {
         useEffect(() => {
                 (myContributionReport.filteredData.length === 0 || myContributionReport.refreshStatus || added) && MyContributionListApi()
         }, []);
-
-        const getMuiTheme = () => createMuiTheme({
-                overrides: {
-                        MuiTableRow: {
-                                root: {
-                                  cursor: "pointer",
-                                  '&.MuiTableRow-hover:hover': {
-                          
-                          
-                                    backgroundColor: "#F4F4FA"
-                          
-                                  }
-                          
-                          
-                          
-                                },
-                                hover: {
-                                  //   "&:hover": {
-                                  //     color: '#2C2799',
-                                  // backgroundColor: " rgba(44,39,153, .05)"
-                                  // }
-                          
-                                }
-                              },
-                              MUIDataTableFilterList: {
-                                chip: {
-                                  display: 'none'
-                                }
-                              },
-                              MuiMenu: {
-                                list: {
-                                  minWidth: "210px"
-                                }
-                              },
-                              MUIDataTableFilter: {
-                                root: {
-                                  backgroundColor: "white",
-                                  width: "80%",
-                                  fontFamily: '"Roboto" ,sans-serif',
-                                },
-                                checkboxFormControl: {
-                                  minWidth: '200px'
-                                }
-                              },
-                              MuiList: {
-                                root: {
-                                  fontFamily: '"Roboto" ,sans-serif',
-                                }
-                          
-                              },
-                        MuiTableCell: {
-                                head: {
-                                  padding: ".6rem .5rem .6rem 1.5rem",
-                                  backgroundColor: "#F8F8FA !important",
-                                  marginLeft: "25px",
-                                  letterSpacing: "0.74",
-                                  fontWeight: "bold",
-                                  minHeight: "700px",
-                                },
-                              },
-                              MuiPaper: {
-                                root: {
-                                  boxShadow: "none !important",
-                                  borderRadius: 0,
-                                  border: "1px solid rgb(224 224 224)",
-                                },
-                              },
-                              MuiDialog: {
-                                paper: { minWidth: "360px", minHeight: "116px" }
-                              },
-                              MuiAppBar: {
-                                root: {
-                                  boxSizing: "none",
-                                  margin: "-1px",
-                                  padding: "0px"
-                                }
-                              },
-                              MUIDataTableToolbar: {
-                                filterPaper: {
-                                  width: "310px"
-                                },
-                                MuiButton: {
-                                  root: {
-                                    display: "none"
-                                  }
-                                }
-                          
-                              },
-                              MuiGrid: {
-                                grid: {
-                          
-                                  maxWidth: "100%"
-                          
-                                }
-                              },
-                              
-                              MuiToolbar: {
-                                root: {
-                                  padding: 0
-                                }
-                              },
-                              MuiFormControlLabel: {
-                                root: {
-                                  height: '36px'
-                                },
-                                label: {
-                                  fontFamily: '"Roboto" ,sans-serif',
-                                  fontSize: '0.875rem'
-                                }
-                          
-                              },
-                          
-                              MUIDataTableBodyCell: {
-                                root: { padding: ".5rem .5rem .5rem .8rem", textTransform: "capitalize" },
-                              },
-                              MuiButton: {
-                                root: {
-                                  minWidth: "25",
-                                  borderRadius: "0",
-                          
-                                },
-                                label: {
-                          
-                                  textTransform: "none",
-                                  fontFamily: '"Segoe UI","Roboto"',
-                                  fontSize: "15px",
-                                  fontWeight: "500",
-                                  lineHeight: "1.14",
-                                  letterSpacing: "0.5px",
-                                  textAlign: "center",
-                                  height: "26px",
-                                  
-                                },
-                                sizeLarge: {
-                                  height: "48px",
-                                },
-                                sizeSmall: {
-                                  height: "36px",
-                                },
-                          
-                          
-                              }
-                          
-                            
-                              
-                        //       MUIDataTableHeadCell: {
-                        //         root: {
-                        //           "&:nth-child(1)": {
-                        //             width: "20%",
-                        //           },
-                        //           "&:nth-child(2)": {
-                        //             width: "18%",
-                        //           },
-                        //           "&:nth-child(3)": {
-                        //             width: "18%",
-                        //           },
-                        //           "&:nth-child(4)": {
-                        //             width: "18%",
-                        //           },
-                        //         },
-                        //       },
-                }
-              })
 
         const MyContributionListApi = () => {
                 dispatch(ClearReport());
@@ -415,7 +253,7 @@ const ContributionList = (props) => {
 
 
                         {view ? (data.length>0 && <GridView data= {data} rowChange={tableRowchange} handleRowClick = {handleRowClick} handleViewChange = {handleViewChange} handleShowFilter = {handleShowFilter} MyContributionListApi= {MyContributionListApi} view= {view} page ={PageInfo.page}handleCardClick = {handleCardClick} handleChangePage = {processTableClickedNextOrPrevious} rowsPerPage = {PageInfo.count}></GridView>)
-                        :  <MuiThemeProvider theme={getMuiTheme()}><MUIDataTable
+                        :  <MuiThemeProvider theme={createMuiTheme}><MUIDataTable
                                 title={`My Contribution`}
                                 data={data}
                                 columns={columns}
