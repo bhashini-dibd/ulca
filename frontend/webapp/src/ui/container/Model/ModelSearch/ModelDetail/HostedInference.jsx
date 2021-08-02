67e0322b545c9313f103398bd058e22242b34e98
Alpana Majhi <alpanamajhi001@gmail.com>
2021-08-02 16:19:54 +0530

    API changes added and snackbar

--- frontend/webapp/src/ui/container/Model/ModelSearch/ModelDetail/HostedInference.jsx
+++ frontend/webapp/src/ui/container/Model/ModelSearch/ModelDetail/HostedInference.jsx
@@ -11,14 +11,25 @@ import {
     CardContent, Card
 } from '@material-ui/core';
 import { useState } from 'react';
+import { identifier } from '@babel/types';
+import Snackbar from '../../../../components/common/Snackbar';
 
 const HostedInference = (props) => {
     const { classes, title, para, modelId } = props;
     const history = useHistory();
     const [translation, setTranslationState] = useState(false)
     const [sourceText, setSourceText] = useState("");
+    const [target, setTarget] = useState("")
+    const [snackbar, setSnackbarInfo] = useState({
+        open: false,
+        message: '',
+        variant: 'success'
+    })
+    const handleSnackbarClose = () => {
+        setSnackbarInfo({ ...snackbar, open: false })
+    }
     const handleCompute = () => {
-        const apiObj = new HostedInferenceAPI(modelId, sourceText);
+        const apiObj = new HostedInferenceAPI(modelId + 1, sourceText);
         fetch(apiObj.apiEndPoint(), {
             method: 'POST',
             headers: apiObj.getHeaders().headers,
@@ -26,17 +37,32 @@ const HostedInference = (props) => {
         }).then(async resp => {
             let rsp_data = await resp.json();
             if (resp.ok) {
-                setTranslationState(true)
+                if (rsp_data.hasOwnProperty('output') && rsp_data.output.length) {
+                    setTarget(rsp_data.output[0].target)
+                    setTranslationState(true)
+                }
             } else {
-                new Promise.reject(rsp_data);
+                setSnackbarInfo({
+                    ...snackbar,
+                    open: true,
+                    message: "The model is not accessible currently. Please try again later",
+                    variant: 'error'
+                })
+                Promise.reject(rsp_data);
             }
         }).catch(err => {
             console.log(err)
+            setSnackbarInfo({
+                ...snackbar,
+                open: true,
+                message: "The model is not accessible currently. Please try again later",
+                variant: 'error'
+            })
         })
     };
     return (
         <div>
-            <Typography className={classes.hosted}>Hosted inference API {< InfoOutlinedIcon className={classes.buttonStyle} fontSize="small" color="disabled" />}</Typography>
+            <Typography variant='h6' className={classes.hosted}>Hosted inference API {< InfoOutlinedIcon className={classes.buttonStyle} fontSize="small" color="disabled" />}</Typography>
             <Grid container spacing={2}>
                 <Grid className={classes.gridCompute} item xl={8} lg={8} md={8} sm={8} xs={8}>
                     <TextField fullWidth
@@ -82,12 +108,21 @@ const HostedInference = (props) => {
                     // <Grid item xl={11} lg={11} md={12} sm={12} xs={12}>
                     <Card style={{ backgroundColor: '#139D601A', color: 'black', heigth: '50px', width: '440px' }}>
                         <CardContent style={{ paddingBottom: '16px' }}>
-                            This is my translated text
+                            {target}
                         </CardContent>
                     </Card>
                     // </Grid>
                 }
             </Grid>
+
+            {snackbar.open &&
+                <Snackbar
+                    open={snackbar.open}
+                    handleClose={handleSnackbarClose}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                    message={snackbar.message}
+                    variant={snackbar.variant}
+                />}
         </div>
 
     )
