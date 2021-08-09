7e0b8680dd97621da23717e9daca69c51b80259e
JainyJoy <jainyjoy97@gmail.com>
2021-08-09 09:44:06 +0530

    model statistics

--- backend/metric/ulca-metric-api/config.py
+++ backend/metric/ulca-metric-api/config.py
@@ -8,8 +8,9 @@ PORT            = 5001
 ENABLE_CORS     = False
 
 # mongodb configs
-MONGO_DB_SCHEMA         =   os.environ.get('MONGO_DB_SCHEMA', 'test')
-MONGO_CONNECTION_URL    =   os.environ.get('MONGO_CLUSTER_URL', 'mongodb://localhost:27017')
+MONGO_DB_SCHEMA         =   os.environ.get('ULCA_MODEL_DB','ulca-process-tracker')
+MONGO_MODEL_COLLECTION  =   os.environ.get('ULCA_MODEL_COL','model')
+MONGO_CONNECTION_URL    =   os.environ.get('ULCA_MONGO_CLUSTER', 'mongodb://localhost:27017')#
 #druid store configs
 DRUID_DB_SCHEMA         =   os.environ.get('MATRIC_DRUID_DB_SCHEMA', 'dataset-training-v5')
 DRUID_CONNECTION_URL    =   os.environ.get('DRUID_CLUSTER_URL', 'druid://localhost:8082/druid/v2/sql/')#druid://localhost:8082/druid/v2/sql/
--- backend/metric/ulca-metric-api/src/db/connection_manager.py
+++ backend/metric/ulca-metric-api/src/db/connection_manager.py
@@ -1,14 +1,55 @@
 import sqlalchemy as db
-from config import MONGO_CONNECTION_URL,MONGO_DB_SCHEMA,DRUID_CONNECTION_URL
-from pymongo import MongoClient
+from config import MONGO_CONNECTION_URL,MONGO_DB_SCHEMA,MONGO_MODEL_COLLECTION,DRUID_CONNECTION_URL
+import pymongo
 import logging
 
-log = logging.getLogger('fie')
-client = MongoClient(MONGO_CONNECTION_URL)
 
+log = logging.getLogger('fie')
 def get_data_store():
     log.info("Establishing connection with druid")
     engine      = db.create_engine(DRUID_CONNECTION_URL)  
     connection  = engine.connect()
     return connection
 
+
+mongo_instance = None
+
+class ModelRepo:
+    
+    def __init__(self):
+       pass
+    #method to instantiate mongo client object
+    def instantiate(self):
+        global mongo_instance
+        client = pymongo.MongoClient(MONGO_CONNECTION_URL)
+        mongo_instance = client[MONGO_DB_SCHEMA][MONGO_MODEL_COLLECTION]
+        return mongo_instance
+
+    #geting the mongo clent object
+    def get_mongo_instance(self):
+        global mongo_instance
+        if not mongo_instance:
+            return self.instantiate()
+        else:
+            return mongo_instance
+
+    def aggregate(self, query):
+        try:
+            col = self.get_mongo_instance()
+            res =   col.aggregate(query) 
+            result = []
+            for record in res:
+                result.append(record)
+            return result
+        except Exception as e:
+            log.exception(f'Exception in repo aggregate: {e}', e)
+            return []
+    
+    def count(self, query):
+        try:
+            col = self.get_mongo_instance()
+            res =   col.count(query) 
+            return res
+        except Exception as e:
+            log.exception(f'Exception in repo search: {e}', e)
+            return []
