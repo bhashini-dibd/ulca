88ffc6d55ba93bac622e603ae07a26574f7d8881
rahul91ranjan <30406102+rahul91ranjan@users.noreply.github.com>
2021-08-25 20:22:08 +0530

    Trigger Benchmark Api

--- backend/api/ulca-dataset-api/src/main/java/com/ulca/dataset/kakfa/Benchmarkconfig/KafkaDatasetIngestPublisherConfig.java
+++ backend/api/ulca-dataset-api/src/main/java/com/ulca/dataset/kakfa/Benchmarkconfig/KafkaDatasetIngestPublisherConfig.java
@@ -1,37 +1,41 @@
-package com.ulca.dataset.kakfa.config;
+package com.ulca.dataset.kakfa.Benchmarkconfig;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.stereotype.Service;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.ulca.dataset.kakfa.model.DatasetIngest;
 
 
-@Service
-public class KafkaDatasetErrorPublisherConfig {
+@Configuration
+public class KafkaDatasetIngestPublisherConfig {
 	
-
 	@Value("${kafka.ulca.bootstrap.server.host}")
     private String bootstrapAddress;
 
 	@Bean
-    public ProducerFactory<String, String> datasetErrorProducerFactory() {
+    public ProducerFactory<String, DatasetIngest> datasetIngestProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
  
     @Bean
-    public KafkaTemplate<String, String> datasetErrorKafkaTemplate() {
-        return new KafkaTemplate<>(datasetErrorProducerFactory());
+    public KafkaTemplate<String, DatasetIngest> datasetIngestKafkaTemplate() {
+        return new KafkaTemplate<>(datasetIngestProducerFactory());
     }
-
 }
+
+
