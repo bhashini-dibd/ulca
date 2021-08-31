46c56e16eeeba6620d8d8456be4614ed2b0d671d
Alpana Majhi <alpanamajhi001@gmail.com>
2021-08-31 17:16:26 +0530

    search fix

--- frontend/webapp/src/redux/actions/api/DataSet/DatasetSearch/SearchAndDownload.js
+++ frontend/webapp/src/redux/actions/api/DataSet/DatasetSearch/SearchAndDownload.js
@@ -8,9 +8,9 @@ export default class SearchAndDownload extends API {
     constructor(timeout = 200000) {
         super("GET", timeout, false);
         this.type = C.GET_SEARCH_OPTIONS;
-        this.endpoint= "https://jsonplaceholder.typicode.com/posts";
+       // this.endpoint= "https://jsonplaceholder.typicode.com/posts";
         this.userDetails = JSON.parse(localStorage.getItem('userInfo'))
-        //this.endpoint = `${CONFIGS.API_URL}${ENDPOINTS.getSearchOptions}`;
+        this.endpoint = `${CONFIGS.API_URL}${ENDPOINTS.getSearchOptions}`;
     }
 
     toString() {
