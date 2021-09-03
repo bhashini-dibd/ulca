62b102e24b740b4496a8fc3018db8b4181c46a0d
sarojsingh2021 <saroj.kumarsingh@tarento.com>
2021-09-03 13:36:27 +0530

    benchmark performance added to mdoel list api
    benchmark performance added to mdoel list api

---
+++ backend/model/api/ulca-model-api/src/main/java/com/ulca/model/response/ModelListResponseDto.java
@@ -0,0 +1,23 @@
+package com.ulca.model.response;
+
+import java.util.List;
+
+import com.ulca.benchmark.model.BenchmarkProcess;
+import com.ulca.model.dao.ModelExtended;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModelListResponseDto extends ModelExtended {
+	
+	List<BenchmarkProcess> benchmarkPerformance ;
+
+}
