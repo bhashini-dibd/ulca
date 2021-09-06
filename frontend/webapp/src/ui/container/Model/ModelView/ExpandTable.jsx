aff2e1cb17162981349642011ee7e1162b27c599
Roshan2810 <rosu285shah@gmail.com>
2021-09-06 07:58:54 +0530

    Sub header bold

--- frontend/webapp/src/ui/container/Model/ModelView/ExpandTable.jsx
+++ frontend/webapp/src/ui/container/Model/ModelView/ExpandTable.jsx
@@ -7,15 +7,19 @@ const ExpandTable = (props) => {
   const { rows, renderStatus, color } = props;
   const renderTable = () => {
     const returnTypo = (value) => {
-      return <Typography variant="body2">{value}</Typography>;
+      return (
+        <Typography variant="body2">
+          <strong>{value}</strong>
+        </Typography>
+      );
     };
 
     return (
       <>
         <TableRow
           style={{
-            borderLeft: `5px solid ${color ? "#E2F2FD" : "#E9F7EF"}`,
-            borderRight: `4px solid ${color ? "#E2F2FD" : "#E9F7EF"}`,
+            borderLeft: `2px solid ${color ? "#E2F2FD" : "#E9F7EF"}`,
+            borderRight: `2px solid ${color ? "#E2F2FD" : "#E9F7EF"}`,
           }}
         >
           <TableCell></TableCell>
