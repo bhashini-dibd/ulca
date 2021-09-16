d102b7159983fdc0c4752911e220f58d90e4a19e
JainyJoy <jainyjoy97@gmail.com>
2021-09-16 14:10:29 +0530

    notifier for data mismatch

--- backend/metric/ulca-utility-service/src/routes/notifier.py
+++ backend/metric/ulca-utility-service/src/routes/notifier.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources import NotifierResource
+from resources import NotifierResource, MismatchNotifierResource
 
 # end-point for independent service
 NOTIFIER_BLUEPRINT = Blueprint("notifier-service", __name__)
 api = Api(NOTIFIER_BLUEPRINT)
+
 api.add_resource(NotifierResource, "/v1/send/mail")
+
+api.add_resource(MismatchNotifierResource, "/v1/notify/mismatch")
