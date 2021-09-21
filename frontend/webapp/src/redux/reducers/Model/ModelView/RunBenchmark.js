7d1690441193085a43fbca3692d6388cb1992cfb
Roshan2810 <rosu285shah@gmail.com>
2021-09-21 16:03:00 +0530

    Multiple selection issue and search issue fix

--- frontend/webapp/src/redux/reducers/Model/ModelView/RunBenchmark.js
+++ frontend/webapp/src/redux/reducers/Model/ModelView/RunBenchmark.js
@@ -55,7 +55,7 @@ const getUpdatedBenchMark = (type, prevState, index, parentIndex = "") => {
   let result = Object.assign([], JSON.parse(JSON.stringify(prevState)));
   if (type === "DATASET") {
     index = getIndex(result.result, index);
-    result.result[index].selected = !result.result[index].selected;
+    // result.result[index].selected = !result.result[index].selected;
     result.filteredData[parentIndex].selected =
       !result.filteredData[parentIndex].selected;
     if (result.selectedIndex.indexOf(index) > -1) {
@@ -77,7 +77,7 @@ const getUpdatedBenchMark = (type, prevState, index, parentIndex = "") => {
       !result.filteredData[parentIndex].metric[index].selected;
   }
   let updatedBenchmarkInfo = [];
-  result.result.forEach((val) => {
+  result.filteredData.forEach((val) => {
     if (val.selected) {
       updatedBenchmarkInfo.push({
         benchmarkId: val.benchmarkId,
@@ -102,6 +102,17 @@ const getUpdatedBenchMark = (type, prevState, index, parentIndex = "") => {
   });
   result.benchmarkInfo = updatedBenchmarkInfo;
   result.submitStatus = getSubmitStatus(updatedBenchmarkInfo);
+  result.result = result.result.map((data) => {
+    let updatedData = {};
+    result.filteredData.forEach((value) => {
+      if (value.benchmarkId === data.benchmarkId) {
+        updatedData = Object.assign({}, JSON.parse(JSON.stringify(value)));
+      } else {
+        updatedData = Object.assign({}, JSON.parse(JSON.stringify(data)));
+      }
+    });
+    return updatedData;
+  });
   return result;
 };
 
@@ -130,7 +141,13 @@ const getFilteredData = (payload, searchValue) => {
   let filteredData = payload.filter((dataset) =>
     dataset.datasetName.toLowerCase().includes(searchValue.toLowerCase())
   );
-  return filteredData;
+  let newSelectedIndex = [];
+  filteredData.forEach((data, i) => {
+    if (data.selected) {
+      newSelectedIndex.push(i);
+    }
+  });
+  return { filteredData, selectedIndex: newSelectedIndex };
 };
 
 const getAvailableFilters = (payload) => {
@@ -197,10 +214,14 @@ const reducer = (state = initialState, action) => {
       };
     }
     case C.SEARCH_BENCHMARK: {
-      getFilteredData(state.result, action.payload);
+      const { filteredData, selectedIndex } = getFilteredData(
+        state.result,
+        action.payload
+      );
       return {
         ...state,
-        filteredData: getFilteredData(state.result, action.payload),
+        filteredData,
+        selectedIndex,
       };
     }
     case C.FILTER_BENCHMARK: {
