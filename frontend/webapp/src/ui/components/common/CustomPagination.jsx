e8e7fbf9a2eb2b0bfa0b51190608d27da2a9f7f0
Roshan2810 <rosu285shah@gmail.com>
2021-09-27 14:40:00 +0530

    Pagination added for benchmark modal

---
+++ frontend/webapp/src/ui/components/common/CustomPagination.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import TableFooter from "@material-ui/core/TableFooter";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import MuiTablePagination from "@material-ui/core/TablePagination";
+import { withStyles } from "@material-ui/core/styles";
+import { TableHead, TableBody } from "@material-ui/core";
+
+const defaultFooterStyles = {};
+
+class CustomFooter extends React.Component {
+  handleRowChange = (event) => {
+    this.props.changeRowsPerPage(event.target.value);
+  };
+
+  handlePageChange = (_, page) => {
+    this.props.changePage(page);
+  };
+
+  render() {
+    const { count, classes, textLabels, rowsPerPage, page } = this.props;
+
+    const footerStyle = {
+      display: "flex",
+      justifyContent: "flex-end",
+      padding: "0px 24px 0px 24px",
+      border: "none",
+    };
+
+    return (
+      <TableFooter>
+        <TableRow style={{ border: "none" }}>
+          <TableCell style={footerStyle} colSpan={19}>
+            <MuiTablePagination
+              component="div"
+              count={count}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              labelRowsPerPage={textLabels.rowsPerPage}
+              labelDisplayedRows={({ from, to, count }) =>
+                `${from}-${to} ${textLabels.displayRows} ${count}`
+              }
+              backIconButtonProps={{
+                "aria-label": textLabels.previous,
+              }}
+              nextIconButtonProps={{
+                "aria-label": textLabels.next,
+              }}
+              rowsPerPageOptions={[10, 20, 100]}
+              onChangePage={this.handlePageChange}
+              onChangeRowsPerPage={this.handleRowChange}
+            />
+          </TableCell>
+        </TableRow>
+      </TableFooter>
+    );
+  }
+}
+
+export default withStyles(defaultFooterStyles, { name: "CustomFooter" })(
+  CustomFooter
+);
