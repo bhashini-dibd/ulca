36619282d45288227035dc5b99085657f695da77
Alpana Majhi <alpanamajhi001@gmail.com>
2021-09-28 13:55:26 +0530

    advance filter changes

--- frontend/webapp/src/redux/reducers/DataSet/DatasetSearch/MySearches.js
+++ frontend/webapp/src/redux/reducers/DataSet/DatasetSearch/MySearches.js
@@ -1,107 +1,106 @@
 import C from "../../../actions/constants";
 import {
-  getLanguageLabel,
-  FilterByDomain,
-  FilterByCollection,
+    getLanguageLabel,
+    FilterByDomain,
+    FilterByCollection,
 } from "../../../../utils/getLabel";
 import getDatasetName from "../../../../utils/getDataset";
 
 const initialState = {
-  responseData: [],
+    responseData: [],
 };
 
 const dateConversion = (value) => {
-  var myDate = new Date(value);
-  let result = myDate.toLocaleString("en-IN", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    hour12: true,
-  });
-  return result.toUpperCase();
+    var myDate = new Date(value);
+    let result = myDate.toLocaleString("en-IN", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true,
+    });
+    return result.toUpperCase();
 };
 
 const getMySearches = (payload) => {
-  let newArr = [];
-  payload.forEach((element) => {
-    if (element.searchCriteria) {
-      let dataSet = getDatasetName(element.searchCriteria.datasetType);
-      let langauge =
-        element.searchCriteria.sourceLanguage &&
-        getLanguageLabel(element.searchCriteria.sourceLanguage).map(
-          (val) => val.label
-        )[0];
-      let tLanguage =
-        element.searchCriteria.targetLanguage &&
-        getLanguageLabel(element.searchCriteria.targetLanguage)
-          .map((val) => val.label)
-          .join(", ");
-      let searchDetails = JSON.parse(
-        element.status.length > 0 && element.status[0].details
-      );
-      let domain =
-        element.searchCriteria.domain &&
-        FilterByDomain(element.searchCriteria.domain)
-          .map((val) => val.label)
-          .join(", ");
-      let collection =
-        element.searchCriteria.collectionMethod &&
-        FilterByCollection(element.searchCriteria.collectionMethod)
-          .map((val) => val.label)
-          .join(", ");
-      newArr.push({
-        sr_no: element.serviceRequestNumber,
-        search_criteria: `${dataSet} | ${langauge} ${
-          tLanguage ? " | " + tLanguage : ""
-        } ${domain ? " | " + domain : ""} ${
-          collection ? " | " + collection : ""
-        }`,
-        searched_on: dateConversion(element.timestamp),
-        status: element.status.length > 0 && element.status[0].status,
+    let newArr = [];
+    payload.forEach((element) => {
+        if (element.searchCriteria) {
+            let dataSet = getDatasetName(element.searchCriteria.datasetType);
+            let langauge =
+                element.searchCriteria.sourceLanguage &&
+                getLanguageLabel(element.searchCriteria.sourceLanguage).map(
+                    (val) => val.label
+                )[0];
+            let tLanguage =
+                element.searchCriteria.targetLanguage &&
+                getLanguageLabel(element.searchCriteria.targetLanguage)
+                    .map((val) => val.label)
+                    .join(", ");
+            let searchDetails = JSON.parse(
+                element.status.length > 0 && element.status[0].details
+            );
+            let domain =
+                element.searchCriteria.domain &&
+                FilterByDomain(element.searchCriteria.domain)
+                    .map((val) => val.label)
+                    .join(", ");
+            let collection =
+                element.searchCriteria.collectionMethod &&
+                FilterByCollection(element.searchCriteria.collectionMethod)
+                    .map((val) => val.label)
+                    .join(", ");
+            newArr.push({
+                sr_no: element.serviceRequestNumber,
+                search_criteria: `${dataSet} | ${langauge} ${tLanguage ? " | " + tLanguage : ""
+                    } ${domain ? " | " + domain : ""} ${collection ? " | " + collection : ""
+                    }`,
+                searched_on: dateConversion(element.timestamp),
+                status: element.status.length > 0 && element.status[0].status,
 
-        count: searchDetails && searchDetails.count,
-        sampleUrl: searchDetails && searchDetails.datasetSample,
-        downloadUrl: searchDetails && searchDetails.dataset,
-        // sourceLanguage: element.searchCriteria.sourceLanguage,
-        // targetLanguage: element.searchCriteria.targetLanguage,
-        // datasetType: element.searchCriteria.datasetType,
-        // domain: element.searchCriteria.domain,
-        // collection: element.searchCriteria.collectionMethod,
-        searchValues: {
-          ...element.searchCriteria,
-          domain: ["general"],
-          collectionSource: "www.pib.org",
-          license: ["mit"],
-          submitterName: "Samantar",
-          collectionMethod: ["auto-aligned"],
-          alignmentTool: ["LaBSE"],
-        },
-      });
-    }
-  });
-  newArr = newArr.reverse();
+                count: searchDetails && searchDetails.count,
+                sampleUrl: searchDetails && searchDetails.datasetSample,
+                downloadUrl: searchDetails && searchDetails.dataset,
+                // sourceLanguage: element.searchCriteria.sourceLanguage,
+                // targetLanguage: element.searchCriteria.targetLanguage,
+                // datasetType: element.searchCriteria.datasetType,
+                // domain: element.searchCriteria.domain,
+                // collection: element.searchCriteria.collectionMethod,
+                searchValues:
+                    // {
+                    element.searchCriteria,
+                //   domain: ["general"],
+                //   collectionSource: "www.pib.org",
+                //   license: ["mit"],
+                //   submitterName: "Samantar",
+                //   collectionMethod: ["auto-aligned"],
+                //   alignmentTool: ["LaBSE"],
+                // },
+            });
+        }
+    });
+    newArr = newArr.reverse();
 
-  return newArr;
+    return newArr;
 };
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case C.GET_MY_REPORT:
-      return {
-        responseData: getMySearches(action.payload),
-      };
-    case C.CLEAR_USER_EVENT:
-      return {
-        ...initialState,
-      };
-    default:
-      return {
-        ...state,
-      };
-  }
+    switch (action.type) {
+        case C.GET_MY_REPORT:
+            return {
+                responseData: getMySearches(action.payload),
+            };
+        case C.CLEAR_USER_EVENT:
+            return {
+                ...initialState,
+            };
+        default:
+            return {
+                ...state,
+            };
+    }
 };
 
 export default reducer;
