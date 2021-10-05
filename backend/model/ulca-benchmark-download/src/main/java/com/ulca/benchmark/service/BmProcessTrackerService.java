c1d26978baff6260c0a844a1751ad7c2baecad8d
sarojsingh2021 <saroj.kumarsingh@tarento.com>
2021-10-05 14:20:40 +0530

    benchmark download code clean
    benchmark download code clean

--- backend/model/ulca-benchmark-download/src/main/java/com/ulca/benchmark/service/BmProcessTrackerService.java
+++ backend/model/ulca-benchmark-download/src/main/java/com/ulca/benchmark/service/BmProcessTrackerService.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ulca.benchmark.dao.BenchmarkProcessDao;
 import com.ulca.benchmark.dao.BenchmarkTaskTrackerDao;
+import com.ulca.benchmark.model.BenchmarkProcess;
 import com.ulca.benchmark.model.BenchmarkTaskTracker;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,58 +20,76 @@ public class BmProcessTrackerService {
 	@Autowired
 	BenchmarkTaskTrackerDao benchmarkTaskTrackerDao;
 	
-public void createTaskTracker(String benchmarkProcessId, BenchmarkTaskTracker.ToolEnum tool, BenchmarkTaskTracker.StatusEnum status) {
-		
+	@Autowired
+	BenchmarkProcessDao benchmarkProcessDao;
+
+	public void createTaskTracker(String benchmarkProcessId, BenchmarkTaskTracker.ToolEnum tool,
+			BenchmarkTaskTracker.StatusEnum status) {
+
 		BenchmarkTaskTracker taskTracker = new BenchmarkTaskTracker();
-		
+
 		taskTracker.setBenchmarkProcessId(benchmarkProcessId);
 		taskTracker.setTool(tool);
 		taskTracker.setStatus(status.toString());
 		taskTracker.setStartTime(new Date().toString());
-		
+
 		benchmarkTaskTrackerDao.save(taskTracker);
-		
+
 	}
 
-public void updateTaskTrackerWithErrorAndEndTime(String benchmarkProcessId, BenchmarkTaskTracker.ToolEnum tool, BenchmarkTaskTracker.StatusEnum status, com.ulca.benchmark.model.BenchmarkError error) {
-	
-	
-	List<BenchmarkTaskTracker> taskTrackerList = benchmarkTaskTrackerDao.findAllByBenchmarkProcessIdAndTool(benchmarkProcessId, tool);
-	
-	if(!taskTrackerList.isEmpty()) {
-		BenchmarkTaskTracker taskTracker = taskTrackerList.get(0);
-		taskTracker.setEndTime(new Date().toString());
-		taskTracker.setLastModified(new Date().toString());
-		taskTracker.setStatus(status.toString());
-		taskTracker.setError(error);
-		benchmarkTaskTrackerDao.save(taskTracker);
-		
-	}else {
-		BenchmarkTaskTracker taskTracker = new BenchmarkTaskTracker();
-		taskTracker.setBenchmarkProcessId(benchmarkProcessId);
-		taskTracker.setTool(tool);
-		taskTracker.setStartTime(new Date().toString());
-		taskTracker.setEndTime(new Date().toString());
-		taskTracker.setLastModified(new Date().toString());
-		taskTracker.setStatus(status.toString());
-		taskTracker.setError(error);
-		benchmarkTaskTrackerDao.save(taskTracker);
-	}
-}
+	public void updateTaskTrackerWithErrorAndEndTime(String benchmarkProcessId, BenchmarkTaskTracker.ToolEnum tool,
+			BenchmarkTaskTracker.StatusEnum status, com.ulca.benchmark.model.BenchmarkError error) {
 
-public void updateTaskTracker(String benchmarkProcessId, BenchmarkTaskTracker.ToolEnum tool, com.ulca.benchmark.model.BenchmarkTaskTracker.StatusEnum status) {
-	
-	List<BenchmarkTaskTracker> taskTrackerList = benchmarkTaskTrackerDao.findAllByBenchmarkProcessIdAndTool(benchmarkProcessId, tool);
-	if(!taskTrackerList.isEmpty()) {
-		BenchmarkTaskTracker taskTracker = taskTrackerList.get(0);
-		if(status == BenchmarkTaskTracker.StatusEnum.completed || status == BenchmarkTaskTracker.StatusEnum.failed) {
+		List<BenchmarkTaskTracker> taskTrackerList = benchmarkTaskTrackerDao
+				.findAllByBenchmarkProcessIdAndTool(benchmarkProcessId, tool);
+
+		if (!taskTrackerList.isEmpty()) {
+			BenchmarkTaskTracker taskTracker = taskTrackerList.get(0);
 			taskTracker.setEndTime(new Date().toString());
+			taskTracker.setLastModified(new Date().toString());
+			taskTracker.setStatus(status.toString());
+			taskTracker.setError(error);
+			benchmarkTaskTrackerDao.save(taskTracker);
+
+		} else {
+			BenchmarkTaskTracker taskTracker = new BenchmarkTaskTracker();
+			taskTracker.setBenchmarkProcessId(benchmarkProcessId);
+			taskTracker.setTool(tool);
+			taskTracker.setStartTime(new Date().toString());
+			taskTracker.setEndTime(new Date().toString());
+			taskTracker.setLastModified(new Date().toString());
+			taskTracker.setStatus(status.toString());
+			taskTracker.setError(error);
+			benchmarkTaskTrackerDao.save(taskTracker);
 		}
-		taskTracker.setStatus(status.toString());
-		benchmarkTaskTrackerDao.save(taskTracker);
-		
 	}
-	
-}
+
+	public void updateTaskTracker(String benchmarkProcessId, BenchmarkTaskTracker.ToolEnum tool,
+			com.ulca.benchmark.model.BenchmarkTaskTracker.StatusEnum status) {
+
+		List<BenchmarkTaskTracker> taskTrackerList = benchmarkTaskTrackerDao
+				.findAllByBenchmarkProcessIdAndTool(benchmarkProcessId, tool);
+		if (!taskTrackerList.isEmpty()) {
+			BenchmarkTaskTracker taskTracker = taskTrackerList.get(0);
+			if (status == BenchmarkTaskTracker.StatusEnum.completed
+					|| status == BenchmarkTaskTracker.StatusEnum.failed) {
+				taskTracker.setEndTime(new Date().toString());
+			}
+			taskTracker.setStatus(status.toString());
+			benchmarkTaskTrackerDao.save(taskTracker);
+
+		}
+
+	}
+
+	public void updateBmProcess(String benchmarkProcessId, String status) {
+		
+		BenchmarkProcess bmProcess = benchmarkProcessDao.findByBenchmarkProcessId(benchmarkProcessId);
+		bmProcess.setStatus(status);
+		bmProcess.setLastModifiedOn(new Date().toString());
+		benchmarkProcessDao.save(bmProcess);
+		
+
+	}
 
 }
