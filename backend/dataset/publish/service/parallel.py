faa14d67f22a58110019dab3a3c4a0ce7e395820
Vishal Mahuli <vishal.mahuli@tarento.com>
2021-10-08 10:55:42 +0530

    notifier integration

--- backend/dataset/publish/service/parallel.py
+++ backend/dataset/publish/service/parallel.py
@@ -358,19 +358,19 @@ class ParallelService:
                 size = sample_size if count > sample_size else count
                 path, path_sample = utils.push_result_to_object_store(result, query["serviceRequestNumber"], size)
                 if path:
-                    op = {"serviceRequestNumber": query["serviceRequestNumber"], "userID": data["userId"],
+                    op = {"serviceRequestNumber": query["serviceRequestNumber"], "userID": query["userId"],
                           "count": count, "dataset": path, "datasetSample": path_sample}
                     pt.task_event_search(op, None)
                 else:
                     log.error(f'There was an error while pushing result to object store!')
                     error = {"code": "OS_UPLOAD_FAILED", "datasetType": dataset_type_parallel, "serviceRequestNumber": query["serviceRequestNumber"],
                                                    "message": "There was an error while pushing result to object store"}
-                    op = {"serviceRequestNumber": query["serviceRequestNumber"], "userID": data["userId"],
+                    op = {"serviceRequestNumber": query["serviceRequestNumber"], "userID": query["userId"],
                           "count": 0, "sample": [], "dataset": None, "datasetSample": None}
                     pt.task_event_search(op, error)
             else:
                 log.info(f'No records retrieved for SRN -- {query["serviceRequestNumber"]}')
-                op = {"serviceRequestNumber": query["serviceRequestNumber"], "userID": data["userId"],
+                op = {"serviceRequestNumber": query["serviceRequestNumber"], "userID": query["userId"],
                       "count": 0, "sample": [], "dataset": None,
                       "datasetSample": None}
                 pt.task_event_search(op, None)
