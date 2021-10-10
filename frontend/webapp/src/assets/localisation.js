942c7f134a917d0c4403232a9f7e05bbdfd700db
Roshan2810 <rosu285shah@gmail.com>
2021-10-10 17:55:57 +0530

    Footer, DialogContent and Dialog translation config added

--- frontend/webapp/src/assets/localisation.js
+++ frontend/webapp/src/assets/localisation.js
@@ -1,5 +1,28 @@
 const localization_EN_Data = {
   "label.sourceLanguage": "Source Language",
+  "label.downloadAll": "Download All",
+  "label.downloadSample": "Download sample",
+  "button.openDialog": "Open dialog",
+  "link.meity": "www.meity.gov.in",
+  "link.contactUs": "contact@bhashini.gov.in",
+  "label.addressInfo":
+    "Electronics Niketan, 6, CGO Complex, Lodhi Road, New Delhi - 110003",
+  "link.joinBhashaDaan": "Join Bhasha Daan",
+  "link.ecosystem": "Ecosystem",
+  "link.whitePaper": "Whitepaper",
+  "label.copyright": "Copyright @2021 NLTM. All Rights Reserved.",
+  "label.nltm": "NLTM: National Language Translation Mission",
+  "label.jsNote":
+    "JavaScript must be enabled to access this site. Supports : Firefox, Google Chrome, Internet Explorer 10.0+, Safari",
+  "label.lastUpdated": "Last reviewed and updated on:16–Jun-2021",
+  "label.technologyDevelopment":
+    "Technology Development for Indian Languages Programme",
+  "link.webInfo": "Web Information Manager",
+  "link.privacyPolicy": "Privacy Policy",
+  "link.termsAndConditions": "Terms of Use",
+  "label.address": "Address",
+  "label.mail": "Mail",
+  "label.web": "Web",
 };
 
 export function translate(locale_text) {
