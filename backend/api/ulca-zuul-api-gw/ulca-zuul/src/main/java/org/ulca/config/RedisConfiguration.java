289ad49c1c17e9c574ab9eeac6883f26536c6f5e
Vishal Mahuli <vishal.mahuli@tarento.com>
2021-10-29 14:46:34 +0530

    redis pass in config

--- backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/config/RedisConfiguration.java
+++ backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/config/RedisConfiguration.java
@@ -2,6 +2,8 @@ package org.ulca.config;
 
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +11,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties("spring.redis")
 public class RedisConfiguration {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private String host;
-    private int port;
+    private String port;
+    private String pass;
 
     @Bean
     RedisClient redisClient() {
-        RedisURI uri = RedisURI.Builder.redis(this.host, this.port)
+        logger.info("host: {}, port: {}, pass: {}", this.host, this.port, this.pass);
+        RedisURI uri = RedisURI.Builder.redis(this.host, Integer.parseInt(this.port))
+                .withPassword(this.pass)
                 .build();
         return RedisClient.create(uri);
     }
@@ -27,11 +33,19 @@ public class RedisConfiguration {
         this.host = host;
     }
 
-    public int getPort() {
+    public String getPort() {
         return port;
     }
 
-    public void setPort(int port) {
+    public void setPort(String port) {
         this.port = port;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
     }
 }
