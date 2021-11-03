9dbb8569c02cc665934938fa687ca7a1e31f28ee
Vishal Mahuli <vishal.mahuli@tarento.com>
2021-11-03 16:20:58 +0530

    added redis config

--- backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/config/RedisConfiguration.java
+++ backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/config/RedisConfiguration.java
@@ -4,7 +4,7 @@ import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -13,21 +13,17 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@ConfigurationProperties("spring.redis")
 public class RedisConfiguration {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private String host;
-    private String port;
-    private String pass;
 
-    @Bean
-    RedisClient redisClient() {
-        logger.info("host: {}, port: {}, pass: {}", this.host, this.port, this.pass);
-        RedisURI uri = RedisURI.Builder.redis(this.host, Integer.parseInt(this.port))
-                .withPassword(this.pass)
-                .build();
-        return RedisClient.create(uri);
-    }
+    @Value(value = "${redis.url}")
+    private String host;
+
+    @Value(value = "${redis.port}")
+    private String port;
+
+    @Value(value = "${redis.pass}")
+    private String pass;
 
     @Bean
     public JedisConnectionFactory connectionFactory() {
@@ -49,6 +45,15 @@ public class RedisConfiguration {
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer( new StringRedisSerializer() );
         return redisTemplate;
+    }
+
+    @Bean
+    RedisClient redisClient() {
+        logger.info("host: {}, port: {}, pass: {}", this.host, this.port, this.pass);
+        RedisURI uri = RedisURI.Builder.redis(this.host, Integer.parseInt(this.port))
+                .withPassword(this.pass)
+                .build();
+        return RedisClient.create(uri);
     }
 
     public String getHost() {
