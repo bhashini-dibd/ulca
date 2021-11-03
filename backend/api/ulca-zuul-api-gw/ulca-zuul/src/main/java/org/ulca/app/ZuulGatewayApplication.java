a1fffb8c78ec9b8052ecef7f60209c320acc673d
Vishal Mahuli <vishal.mahuli@tarento.com>
2021-11-03 16:28:45 +0530

    added redis config

--- backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/app/ZuulGatewayApplication.java
+++ backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/app/ZuulGatewayApplication.java
@@ -1,5 +1,12 @@
 package org.ulca.app;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.ulca.cache.ZuulConfigCache;
 import org.ulca.filters.error.ErrorFilterFilter;
 import org.ulca.filters.post.ResponseFilter;
@@ -27,6 +34,37 @@ public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+    @Value(value = "${redis.url}")
+    private String host;
+
+    @Value(value = "${redis.port}")
+    private String port;
+
+    @Value(value = "${redis.pass}")
+    private String pass;
+
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        logger.info("host: {}, port: {}, pass: {}", host, port, pass);
+        configuration.setHostName(host);
+        configuration.setPort(Integer.parseInt(port));
+        configuration.setPassword(pass);
+        configuration.setDatabase(10);
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(connectionFactory());
+        redisTemplate.setKeySerializer( new StringRedisSerializer() );
+        redisTemplate.setValueSerializer(new StringRedisSerializer() );
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer( new StringRedisSerializer() );
+        return redisTemplate;
     }
 
     @Autowired
@@ -66,4 +104,35 @@ public class ZuulGatewayApplication {
 
     @Bean
     public ResponseFilter responseFilter() {return new ResponseFilter();}
+
+    @Bean
+    RedisClient redisClient() {
+        logger.info("host: {}, port: {}, pass: {}", this.host, this.port, this.pass);
+        RedisURI uri = RedisURI.Builder.redis(this.host, Integer.parseInt(this.port))
+                .withPassword(this.pass)
+                .build();
+        return RedisClient.create(uri);
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) { this.host = host; }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
