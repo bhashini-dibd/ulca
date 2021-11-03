ed20693c47210b0d30bbb1a308431b044845954f
Vishal Mahuli <vishal.mahuli@tarento.com>
2021-11-03 16:05:24 +0530

    added redis config

--- backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/config/RedisConfiguration.java
+++ backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/config/RedisConfiguration.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @ConfigurationProperties("spring.redis")
@@ -25,13 +29,33 @@ public class RedisConfiguration {
         return RedisClient.create(uri);
     }
 
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        logger.info("host: {}, port: {}, pass: {}", host, port, pass);
+        configuration.setHostName(host);
+        configuration.setPort(Integer.parseInt(port));
+        configuration.setPassword(pass);
+        configuration.setDatabase(10);
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(connectionFactory());
+        redisTemplate.setKeySerializer( new StringRedisSerializer() );
+        redisTemplate.setValueSerializer(new StringRedisSerializer() );
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer( new StringRedisSerializer() );
+        return redisTemplate;
+    }
+
     public String getHost() {
         return host;
     }
 
-    public void setHost(String host) {
-        this.host = host;
-    }
+    public void setHost(String host) { this.host = host; }
 
     public String getPort() {
         return port;
