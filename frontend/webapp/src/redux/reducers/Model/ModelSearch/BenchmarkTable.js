a7cdccaf0a758ce96040d4ed3bbaa81a8eb4f555
Roshan2810 <rosu285shah@gmail.com>
2021-11-15 12:21:53 +0530

    Benchmark dataset date sorting issue fix

--- frontend/webapp/src/redux/reducers/Model/ModelSearch/BenchmarkTable.js
+++ frontend/webapp/src/redux/reducers/Model/ModelSearch/BenchmarkTable.js
@@ -1,68 +1,40 @@
 import C from "../../../actions/constants";
 
 const initialState = {
-    benchmarkPerformance: [],
-    //   benchmarkId: "",
-    //   name: "",
-    //   description: "",
-    //   metric: null,
-    //   dataset: "",
-    //   domain: [],
-    //   task: "",
-    //   languages: [],
-    //   createdOn: null,
-    //   submittedOn: null,
+  benchmarkPerformance: [],
 };
 
-const updateDate=(data)=>{
-    return data.map(value=>{
-        value.createdOn = dateConversion(value.createdOn)
-        return value;
-    })
-}
+const updateDate = (data) => {
+  return data.map((value) => {
+    value.createdOn = dateConversion(value.createdOn);
+    return value;
+  });
+};
 
 const getBenchmarkTableDetails = (data) => {
-    data.benchmarkPerformance = updateDate(data.benchmarkPerformance);
-    return {
-        // description: data.description,
-        // refUrl: data.dataset,
-        // language: `${data.languages[0].sourceLanguage} - ${data.languages[0].targetLanguage}`,
-        // domain: data.domain ? data.domain.join(", ") : "",
-        // modelName: data.name,
-        // metric: data.metric ? data.metric.join(", ") : "",
-        // task: data.task.type,
-        // metricArray: data.metric,
-        benchmarkPerformance: data.benchmarkPerformance,
-    };
+  data.benchmarkPerformance = updateDate(data.benchmarkPerformance);
+  return {
+    benchmarkPerformance: data.benchmarkPerformance,
+  };
 };
 
 const dateConversion = (value) => {
-    var myDate = new Date(value);
-    let result = myDate.toLocaleString("en-IN", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-      hour12: true,
-    });
-    return result.toUpperCase();
-  };
-  
+  var myDate = new Date(value);
+  return myDate.getTime();
+};
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case C.BENCHMARK_TABLE:
-        const data = getBenchmarkTableDetails(action.payload);
-        return {
-          ...data,
-        };
-      default:
-        return {
-          ...state,
-        };
-    }
-  };
+  switch (action.type) {
+    case C.BENCHMARK_TABLE:
+      const data = getBenchmarkTableDetails(action.payload);
+      return {
+        ...data,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
 
 export default reducer;
