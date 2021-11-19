c6d5edc2f45f062963c32bdee909e3684c362b86
rahul91ranjan <30406102+rahul91ranjan@users.noreply.github.com>
2021-11-19 12:44:17 +0530

    changes of benchmark submit error

--- backend/model/api/ulca-model-api/src/main/java/com/ulca/benchmark/controller/BenchmarkController.java
+++ backend/model/api/ulca-model-api/src/main/java/com/ulca/benchmark/controller/BenchmarkController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.mongodb.DuplicateKeyException;
 import com.ulca.benchmark.constant.BenchmarkConstants;
 import com.ulca.benchmark.request.BenchmarkListByModelRequest;
 import com.ulca.benchmark.request.BenchmarkSearchRequest;
