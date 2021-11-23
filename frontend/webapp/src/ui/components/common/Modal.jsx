28c9c5751feaf795ddc99d3c1d87cd1fb26532e7
Roshan2810 <rosu285shah@gmail.com>
2021-11-23 23:15:22 +0530

    Modal body created

--- frontend/webapp/src/ui/components/common/Modal.jsx
+++ frontend/webapp/src/ui/components/common/Modal.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
@@ -19,12 +15,15 @@ function getModalStyle() {
 
 const useStyles = makeStyles((theme) => ({
   paper: {
+    borderRadius: "20px",
     position: "absolute",
-    width: 400,
+    width: "35vw",
     backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
+    "@media (max-width: 1000px)": {
+      width: "65vw",
+    },
   },
 }));
 
--- frontend/webapp/src/ui/container/Admin/UpdateUserInfo.jsx
+++ frontend/webapp/src/ui/container/Admin/UpdateUserInfo.jsx
@@ -1,12 +1,98 @@
+import {
+  Grid,
+  TextField,
+  Typography,
+  Button,
+  IconButton,
+  Tooltip,
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
 import Modal from "../../components/common/Modal";
+import AdminPanelStyle from "../../styles/AdminPanel";
+import Autocomplete from "../../components/common/Autocomplete";
+import CloseIcon from "@material-ui/icons/Close";
 
 const UpdateUserInfo = (props) => {
-  const { open, handleClose } = props;
+  //fetching props
+  const { open, handleClose, classes } = props;
+
+  // function returning TextField
+
+  const renderTextField = (label) => {
+    return (
+      <TextField fullWidth variant="outlined" color="primary" label={label} />
+    );
+  };
+
   return (
     <Modal open={open} handleClose={handleClose}>
-      <p>This is a Modal</p>
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={11}
+          sm={11}
+          md={11}
+          lg={11}
+          xl={11}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <Typography variant="h5">Edit Profile</Typography>
+        </Grid>
+        <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+          <IconButton onClick={handleClose}>
+            <Tooltip placement="right" title="Close">
+              <CloseIcon fontSize="small" />
+            </Tooltip>
+          </IconButton>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          {renderTextField("User ID")}
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          {renderTextField("Name")}
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          {renderTextField("Password")}
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          {renderTextField("Confirm Password")}
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Autocomplete
+            id="org"
+            label="Organization"
+            options={[{ label: "IIT Madras", value: "iitMadras" }]}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Autocomplete
+            id="role"
+            label="Role"
+            options={[
+              { label: "Contributor", value: "contributor" },
+              { label: "Benchmark Dataset Contributor", value: "bmd" },
+            ]}
+          />
+        </Grid>
+        <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+          <Button fullWidth size="large" color="default" variant="contained">
+            Clear
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+          <Button
+            onClick={handleClose}
+            fullWidth
+            size="large"
+            color="primary"
+            variant="contained"
+          >
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
     </Modal>
   );
 };
 
-export default UpdateUserInfo;
+export default withStyles(AdminPanelStyle)(UpdateUserInfo);
