7d266139625bbe1a00a0cdb6a3de7543d802d85a
Roshan2810 <rosu285shah@gmail.com>
2021-12-03 11:28:14 +0530

    homepage path change

--- frontend/webapp/src/web.routes.js
+++ frontend/webapp/src/web.routes.js
@@ -2,14 +2,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import history from "./web.history";
 import Dashboard from "./Layout";
 export default function App() {
+  console.log(process.env.PUBLIC_URL);
   return (
     <Router history={history} basename="">
       <div>
         <Switch>
-          <Route
-            exact={`${process.env.PUBLIC_URL}/ulca/intro`}
-            component={Dashboard}
-          />
           <Route
             exact={`${process.env.PUBLIC_URL}/intro`}
             component={Dashboard}
