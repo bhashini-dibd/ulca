df538b2aae2ecc245adc4fe5c07e88dbc0b847b5
Roshan2810 <rosu285shah@gmail.com>
2021-12-03 16:19:48 +0530

    Component changed

--- frontend/webapp/src/index.js
+++ frontend/webapp/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './web.routes';
+import App from './Layout';
 import reportWebVitals from './reportWebVitals';
 import Theme from "./theme/theme-default";
 import { MuiThemeProvider } from "@material-ui/core";
