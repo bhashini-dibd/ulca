d4e545574dfb0cca8f6538092a05c217aba113e3
Roshan2810 <rosu285shah@gmail.com>
2021-12-06 15:05:11 +0530

    dev home page added

--- frontend/webapp/homepage.js
+++ frontend/webapp/homepage.js
@@ -2,7 +2,7 @@ let packageJson = require("./package.json");
 var shell = require("shelljs");
 const file = require("fs");
 let parsedJson = JSON.parse(JSON.stringify(packageJson));
-parsedJson.homepage = `${process.env.PUBLIC_URL}/ulca`;
+parsedJson.homepage = `https://dev.ulcacontrib.org/ulca`;
 packageJson = JSON.stringify(parsedJson);
 file.writeFileSync("./package.json", packageJson);
 shell.exec("react-scripts build");
