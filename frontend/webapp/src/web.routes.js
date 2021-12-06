947540413231519b4161fb185a7af36a244eef26
Roshan2810 <rosu285shah@gmail.com>
2021-12-06 17:21:34 +0530

    Updated basename

--- frontend/webapp/src/web.routes.js
+++ frontend/webapp/src/web.routes.js
@@ -69,12 +69,12 @@ const PrivateRoute = ({
 
 export default function App() {
   return (
-    <Router history={history} basename="ulca/">
+    <Router history={history} basename="/">
       <div>
         <Switch>
           <Route
             exact
-            path={`${process.env.PUBLIC_URL}`}
+            path={`${process.env.PUBLIC_URL}/`}
             component={Dashboard}
           />
           <Route
