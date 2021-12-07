5bc4f316ca0178bdf56dee5107c1301eca522957
JainyJoy <jainyjoy97@gmail.com>
2021-12-07 12:43:54 +0530

    updating url

--- cron-jobs/ulca-eta-cron-service/src/routes/service_routes.py
+++ cron-jobs/ulca-eta-cron-service/src/routes/service_routes.py
@@ -5,5 +5,5 @@ from resources import ETACalculatorResource, ETAResource
 # end-point for independent service
 CRON_BLUEPRINT = Blueprint("cron-manager-service", __name__)
 api = Api(CRON_BLUEPRINT)
-api.add_resource(ETACalculatorResource, "/v1/get/estimate")
+api.add_resource(ETACalculatorResource, "/v1/calculate/time")
 api.add_resource(ETAResource, "/v1/estimates/get/values")
