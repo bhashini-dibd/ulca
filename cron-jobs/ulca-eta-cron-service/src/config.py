e157da6c5776127e1cd6371a061ac194a3c10aad
Jainy Joy <52598380+JainyJoy@users.noreply.github.com>
2021-12-07 13:22:54 +0530

    Update config.py

--- cron-jobs/ulca-eta-cron-service/src/config.py
+++ cron-jobs/ulca-eta-cron-service/src/config.py
@@ -11,6 +11,6 @@ ulca_db_cluster           =   os.environ.get('ULCA_MONGO_CLUSTER', 'mongodb://lo
 process_db                =   os.environ.get('ULCA_PROC_TRACKER_DB', 'ulca-process-tracker')
 process_collection        =   os.environ.get('ULCA_PROC_TRACKER_PROC_COL', 'ulca-pt-processes')
 eta_collection            =   os.environ.get('ULCA_PROC_TRACKER_ETA_COL', 'ulca-pt-estimates') 
-eta_cron_interval_sec     =   os.environ.get('ETA_CRON_INTERVAL_SEC',17800)#86400
+eta_cron_interval_sec     =   os.environ.get('ETA_CRON_INTERVAL_SEC',500)#86400
 
 
