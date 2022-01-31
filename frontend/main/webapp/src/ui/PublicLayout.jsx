c378e830670c2fa5ed268c96f83773ab135a93fe
Roshan2810 <rosu285shah@gmail.com>
2022-01-31 12:20:46 +0530

    Lazy loading added for public page

--- frontend/main/webapp/src/ui/PublicLayout.jsx
+++ frontend/main/webapp/src/ui/PublicLayout.jsx
@@ -1,35 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense } from "react";
 
-import { useSelector } from "react-redux";
-
-import { useHistory } from "react-router-dom";
-import Header from "./components/common/Header";
-import Footer from "./components/common/Footer";
 import Theme from "./theme/theme-default";
 import { withStyles, MuiThemeProvider } from "@material-ui/core/styles";
 import GlobalStyles from "./styles/Styles";
-import Spinner from "./components/common/Spinner";
-import Snackbar from './components/common/Snackbar';
 
+const Header = React.lazy(() => import("./components/common/Header"));
+const Footer = React.lazy(() => import("./components/common/Footer"));
 
 function App(props) {
   const Component = props.component;
-  const { classes,type,index  } = props;
+  const { type, index } = props;
 
-  const handleClose = () =>{
-    setPopup(false)
-  }
-
-  
+  const fallback = () => <div>Loading...</div>;
 
   return (
     <MuiThemeProvider theme={Theme}>
-            <Header type={type} index={index} style={{ marginBottom: "10px" }} />
-          <Component />
-          <Footer/>
-        </MuiThemeProvider>
-          
-       
+      <Suspense fallback={fallback}>
+        <Header type={type} index={index} style={{ marginBottom: "10px" }} />
+      </Suspense>
+      <Component />
+      <Suspense fallback={fallback}>
+        <Footer />
+      </Suspense>
+    </MuiThemeProvider>
   );
 }
 export default withStyles(GlobalStyles(Theme), { withTheme: true })(App);
