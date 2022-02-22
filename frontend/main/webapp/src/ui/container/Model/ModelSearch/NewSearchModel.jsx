8dff2073eeba901e97e04a3e7ffff6f8d4a81c64
Alpana Majhi <alpanamajhi001@gmail.com>
2022-02-22 14:49:14 +0530

    Explore model sequence changes

--- frontend/main/webapp/src/ui/container/Model/ModelSearch/NewSearchModel.jsx
+++ frontend/main/webapp/src/ui/container/Model/ModelSearch/NewSearchModel.jsx
@@ -38,7 +38,7 @@ function TabPanel(props) {
 const NewSearchModel = () => {
   const filter = useSelector((state) => state.searchFilter);
   const type = ModelTask.map((task) => task.value);
-  const [value, setValue] = useState(type.indexOf(filter.type));
+  const [value, setValue] = useState(0);
   const { searchValue } = useSelector((state) => state.BenchmarkList);
   const [anchorEl, setAnchorEl] = useState(null);
   const popoverOpen = Boolean(anchorEl);
