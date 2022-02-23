d7f8102f4b2a06f20a3ccb80e2c767f899b8aaf6
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2022-02-23 13:11:27 +0530

    css changes

--- frontend/main/webapp/src/ui/components/common/SubHeader.jsx
+++ frontend/main/webapp/src/ui/components/common/SubHeader.jsx
@@ -111,7 +111,7 @@ const SubHeader = (props) => {
                 </Grid>
                 <Grid item >
                    
-                    <Tabs value={props.value}  onChange={props.handleChange}  value={false} indicatorColor="primary" textColor="primary" variant="scrollable"scrollButtons="auto">
+                    <Tabs value={props.value}  onChange={props.handleChange}  indicatorColor="primary" textColor="primary" variant="scrollable" scrollButtons="auto"  aria-label="scrollable prevent tabs example">
                         {
                             props.tabs.map((tab, index) => {
                                 if(tab.roles.includes(role) ||(tab.public)){
