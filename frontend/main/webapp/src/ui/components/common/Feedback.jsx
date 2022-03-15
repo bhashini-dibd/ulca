08a6261a7fd7704cf0499d939d322126b18f3489
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2022-03-15 13:35:37 +0530

    feedback ui

--- frontend/main/webapp/src/ui/components/common/Feedback.jsx
+++ frontend/main/webapp/src/ui/components/common/Feedback.jsx
@@ -6,6 +6,12 @@ import ThumbDownAltOutlinedIcon from '@material-ui/icons/ThumbDownAltOutlined';
 import { makeStyles ,withStyles} from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
 import Popover from '@material-ui/core/Popover';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+
+
 const useStyles = makeStyles((theme) => ({
   typography: {
     padding: theme.spacing(1),
@@ -59,7 +65,9 @@ const useStyles = makeStyles((theme) => ({
  function SimpleDialogDemo() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorE2, setAnchorE2] = React.useState(null);
   const [value, setValue] = React.useState(0);
+ 
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -68,10 +76,21 @@ const useStyles = makeStyles((theme) => ({
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const handleClosefeedback = () =>{
+    setAnchorE2(null);
+  }
+  const handleClickfeedback = (event) => {
+    handleClose();
+    setAnchorE2(event.currentTarget);
+  };
 
+  const open1 = Boolean(anchorE2);
+  const id1 = open1 ? 'simple-popover' : undefined;
+  
+  
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
-
+  console.log(anchorEl)
   return (
     <div>
       <Button  variant="contained" size="small" className={classes.feedbackbutton} onClick={handleClick}>
@@ -93,6 +112,7 @@ const useStyles = makeStyles((theme) => ({
           horizontal: 'right',
         }}
       >
+      
        
         
      
@@ -109,16 +129,74 @@ const useStyles = makeStyles((theme) => ({
           }}
          
         />
-    
      < Typography  className={classes.feedbacktypography} variant="body2"  > very bad   < Typography  variant="body2"  style={{ float: "right",fontSize:"12px"}} > very good </Typography>   </Typography>
    
+     <div className={classes.root}>
+      
+      <Grid container justifyContent="center">
+        <Grid item>
+         
+        <Link
+          component="button"
+          variant="body2"
+          onClick={handleClickfeedback}
+>
+  detailed Feedback
+</Link>
+
+        </Grid>
+      </Grid>
+      
+    </div>
+
   <Button variant="outlined" size="small"  color="primary"  className={classes.submitbutton}  >
          Submit
     </Button>
+    
    <Typography  className={classes.typography} align="center" variant="body2" component="div"  style={{fontSize:"12px"}}>
       Your feedback will be used to help to improve the product</Typography> 
       
    
+      </Popover>
+      <Popover
+       style={{maxwidth:"900px"}}
+        id={id1}
+        open={open1}
+        anchorE2={anchorE2}
+        onClose={handleClosefeedback}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: '',
+          horizontal: 'right',
+        }}
+      
+      >
+        <Typography className={classes.typography}>Please rate your experience...</Typography>
+        <Box  p={2}  style={{padding:"10px"}}>
+              
+              <Typography  variant="body2" style={{marginTop:"10px"}}>Rate Speech to Text Quality</Typography>
+              <Rating name="size-medium" />
+              <Button style={{float:"right"}} variant="outlined" size="small" color="primary" >
+          Suggest an edit
+        </Button>
+              <Typography  variant="body2" style={{marginTop:"10px"}}>Rate Translate  Text Quality</Typography>
+              <Rating name="size-medium" />
+              <Button  style={{float:"right"}} variant="outlined" size="small" color="primary" >
+          Suggest an edit
+        </Button>
+              <Typography  variant="body2" style={{marginTop:"10px"}}>Rate Translated Speech Quality Quality</Typography>
+              <Rating name="size-medium" />
+            </Box>
+            <Typography  variant="body2">Add your comments</Typography>
+            <TextareaAutosize aria-label="empty textarea" />
+            <div>
+            <Button variant="outlined" size="small" color="primary" >
+          Submit
+        </Button>
+        </div>
       </Popover>
     </div>
   );
