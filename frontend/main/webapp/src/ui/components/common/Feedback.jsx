5233336907a1931e5f6e545768b9586e3bf7f884
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2022-03-17 11:57:54 +0530

    feedback ui changes

--- frontend/main/webapp/src/ui/components/common/Feedback.jsx
+++ frontend/main/webapp/src/ui/components/common/Feedback.jsx
@@ -1,10 +1,8 @@
-import React ,{useState}from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbDownAltIcon from '@material-ui/icons/ThumbDownAlt';
-import { makeStyles ,withStyles} from '@material-ui/core/styles';
-import Rating from '@material-ui/lab/Rating';
 import Popover from '@material-ui/core/Popover';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
@@ -14,27 +12,28 @@ import TextField from '@material-ui/core/TextField';
 import FeedbackStyle from "../../styles/Feedback";
 import { translate } from "../../../assets/localisation";
 import '../../styles/css/GlobalCssSlider.css';
+import { StyledRating } from './StyledRating';
+import { withStyles } from '@material-ui/core/styles';
 
 
- function SimpleDialogDemo(props) {
-  const { classes} = props;
+function SimpleDialogDemo(props) {
+  const { classes } = props;
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [anchorE2, setAnchorE2] = React.useState(null);
   const [value, setValue] = React.useState(0);
- 
-  const iconStyle = {
-    width: 100,
-    "&:hover": {
-    
-      backgroundColor: "#FFFF"
-    }
-   
- }
- const smallDistanceStyle = {
-  width: 100,
-  height: 100,
-  padding: 50
-}
+  const [detailedFeedback, setDetailedFeedback] = useState(false);
+
+  // const iconStyle = {
+  //   width: 100,
+  //   "&:hover": {
+  //     backgroundColor: "#FFFFFF"
+  //   },
+  // }
+  // const smallDistanceStyle = {
+  //   width: 100,
+  //   height: 100,
+  //   padding: 50
+  // }
 
 
   const handleClick = (event) => {
@@ -44,7 +43,7 @@ import '../../styles/css/GlobalCssSlider.css';
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const handleClosefeedback = () =>{
+  const handleClosefeedback = () => {
     setAnchorE2(null);
   }
   const handleClickfeedback = (event) => {
@@ -54,8 +53,8 @@ import '../../styles/css/GlobalCssSlider.css';
 
   const open1 = Boolean(anchorE2);
   const id1 = open1 ? 'simple-popover' : undefined;
-  
-  
+
+
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
   console.log(anchorEl)
@@ -64,80 +63,76 @@ import '../../styles/css/GlobalCssSlider.css';
     display: 'flex',
     alignItems: 'center'
   };
+
+  const handleRatingChange = (event, newValue) => {
+    setValue(newValue);
+    if (newValue <= 3)
+      setDetailedFeedback(true);
+    else
+      setDetailedFeedback(false);
+  }
+
   return (
-    
+
     <div >
-      <Button  variant="contained" size="small" className={classes.feedbackbutton} onClick={handleClick}>
-       <ThumbUpAltIcon  className={classes.feedbackIcon} />
-      < ThumbDownAltIcon   className={classes.feedbackIcon}  />
-       <Typography variant="body2"  className={classes.feedbackTitle} > {translate("button:feedback")}</Typography>
-       </Button>
+      <Button variant="contained" size="small" className={classes.feedbackbutton} onClick={handleClick}>
+        <ThumbUpAltIcon className={classes.feedbackIcon} />
+        < ThumbDownAltIcon className={classes.feedbackIcon} />
+        <Typography variant="body2" className={classes.feedbackTitle} > {translate("button:feedback")}</Typography>
+      </Button>
       <Popover
         id={id}
         open={open}
         anchorEl={anchorEl}
         onClose={handleClose}
-         anchorOrigin={{
-           vertical: '',
+        anchorOrigin={{
+          vertical: '',
           horizontal: 'right',
-         }}
+        }}
         transformOrigin={{
           vertical: 'bottom',
           horizontal: 'right',
         }}
       >
-      
-       
-        
-     
-      <Typography className={classes.typography} align="center" >   {translate("lable.feedback1")} <br/>  {translate("lable.feedbacks")}</Typography>
-     
-      
-        <Rating
-       itemStyle={smallDistanceStyle} 
-      itemIconStyle={iconStyle}
-        className={classes.rating}
+
+        <Typography className={classes.typography} align="center" >   {translate("lable.feedback1")} <br />  {translate("lable.feedbacks")}</Typography>
+
+        <StyledRating
+          className={classes.rating}
           size="large"
           name="simple-controlled"
           value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-         
+          onChange={handleRatingChange}
+
         />
-     < Typography  className={classes.feedbacktypography} variant="body2"  >  {translate("lable.verybad")}  < Typography  variant="body2"  style={{ float: "right",fontSize:"12px"}} >  {translate("lable.verygood")}  </Typography>   </Typography>
-   
-     <div className={classes.root}>
-      
-      <Grid container justifyContent="center">
-        <Grid item>
-         
-        <Link
-          component="button"
-          variant="body2"
-          onClick={handleClickfeedback}
-          style={{color:"#FD7F23",fontSize:"13px",textDecoration: "underline"}}
->
-{translate("link.feedback")}
-</Link>
+        < Typography className={classes.feedbacktypography} variant="body2"  >  {translate("lable.verybad")}  < Typography variant="body2" style={{ float: "right", fontSize: "12px" }} >  {translate("lable.verygood")}  </Typography>   </Typography>
 
-        </Grid>
-      </Grid>
-      
-    </div>
+        <div className={classes.root}>
 
-  <Button variant="outlined" size="small"  color="primary"  className={classes.submitbutton}  >
-  {translate("button.submit")}
-    </Button>
-    
-   <Typography  className={classes.typographys} align="center" variant="body2" component="div" >
-   {translate("lable.feedback2")}</Typography> 
-      
-   
+          <Grid container justifyContent="center">
+            <Grid item>
+
+              {detailedFeedback ? <Link
+                component="button"
+                variant="body2"
+                onClick={handleClickfeedback}
+                style={{ color: "#FD7F23", fontSize: "13px", textDecoration: "underline" }}
+              >
+                {translate("link.feedback")}
+              </Link> : <></>}
+
+            </Grid>
+          </Grid>
+        </div>
+        <Button variant="outlined" size="small" color="primary" className={classes.submitbutton}  >
+          {translate("button.submit")}
+        </Button>
+
+        <Typography className={classes.typographys} align="center" variant="body2" component="div" >
+          {translate("lable.feedback2")}</Typography>
       </Popover>
-     
+
       <Popover
-     
         id={id1}
         open={open1}
         anchorE2={anchorE2}
@@ -155,51 +150,49 @@ import '../../styles/css/GlobalCssSlider.css';
         }}
         anchorReference="anchorPosition"
         anchorPosition={{ top: 214, left: 1148, }}
-        
-      
       >
         <Typography variant="body2" className={classes.typography2}> {translate("lable.feedback3")}</Typography>
-        <Box  p={5}>
-              
-              <Typography  variant="body2"  className={classes.typography1}>Rate  <span  style={{fontWeight:"bold"}}>Speech to Text</span> Quality</Typography>
-              <Rating name="size-medium" />
-              <Button className={classes.buttonsuggest}  variant="outlined" size="small" color="primary" >
-             <Typography variant="body2"  color="primary" > {translate("button.Suggest an edit")}</Typography>   
-          
-        </Button>
-              <Typography  variant="body2" className={classes.typography1}>Rate <span style={{fontWeight:"bold"}}  >Translate  Text</span>  Quality</Typography>
-              <Rating  name="size-medium" />
-              <Button   variant="outlined" size="small" color="primary"  className={classes.buttonsuggest}>
-                <Typography variant="body2"  color="primary">  {translate("button.Suggest an edit")}</Typography>
-          
-        </Button>
-              <Typography  variant="body2" className={classes.typography1} >Rate  <span  style={{fontWeight:"bold"}}>Translated Speech</span> Quality </Typography>
-              <Rating name="size-medium" />
-            </Box>
-            <div style={{  borderBottom:"1px solid #ECE7E6 ", width:"240px", margin:"auto" ,paddingBottom:"20px"}}></div>
-           
-            <Typography  variant="body2" style={{margin:"10px 10px 10px 10px"}}> {translate("lable.feedback4")}</Typography>
-            <Grid container justifyContent="center">
-              <Grid item>
+        <Box p={5}>
+
+          <Typography variant="body2" className={classes.typography1}>Rate  <span style={{ fontWeight: "bold" }}>Speech to Text</span> Quality</Typography>
+          <StyledRating name="size-medium" />
+          <Button className={classes.buttonsuggest} variant="outlined" size="small" color="primary" >
+            <Typography variant="body2" color="primary" > {translate("button.Suggest an edit")}</Typography>
+
+          </Button>
+          <Typography variant="body2" className={classes.typography1}>Rate <span style={{ fontWeight: "bold" }}  >Translate  Text</span>  Quality</Typography>
+          <StyledRating name="size-medium" />
+          <Button variant="outlined" size="small" color="primary" className={classes.buttonsuggest}>
+            <Typography variant="body2" color="primary">  {translate("button.Suggest an edit")}</Typography>
+
+          </Button>
+          <Typography variant="body2" className={classes.typography1} >Rate  <span style={{ fontWeight: "bold" }}>Translated Speech</span> Quality </Typography>
+          <StyledRating name="size-medium" />
+        </Box>
+        <div style={{ borderBottom: "1px solid #ECE7E6 ", width: "240px", margin: "auto", paddingBottom: "20px" }}></div>
+
+        <Typography variant="body2" style={{ margin: "10px 10px 10px 10px" }}> {translate("lable.feedback4")}</Typography>
+        <Grid container justifyContent="center">
+          <Grid item>
             <TextareaAutosize
-               aria-label="minimum height"
-               minRows={4}
-               className={classes.textareaAutosize}
-               style={{ width: 250  }}
-    />
-    
+              aria-label="minimum height"
+              minRows={4}
+              className={classes.textareaAutosize}
+              style={{ width: 250 }}
+            />
+
+          </Grid>
+          <Grid container justifyContent="center">
+            <Grid items>
+              <Button variant="outlined" size="small" color="primary" style={{ margin: "10px" }}  >
+                {translate("button.submit")}
+              </Button>
             </Grid>
-            <Grid container justifyContent="center">
-         <Grid items>
-            <Button variant="outlined" size="small" color="primary" style={{margin:"10px"}}  >
-            {translate("button.submit")}
-        </Button>
-        </Grid>
-        </Grid>
+          </Grid>
         </Grid>
       </Popover>
-    
+
     </div>
   );
 }
-export default  withStyles(FeedbackStyle) (SimpleDialogDemo);
+export default withStyles(FeedbackStyle)(SimpleDialogDemo);
