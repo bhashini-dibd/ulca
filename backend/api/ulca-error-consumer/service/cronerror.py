6f5cfba6203e3305efbac332f343e8b3c58d0ec4
Siddanthshaiva <siddanth.shaiva@gmail.com>
2022-03-30 13:37:53 +0530

    error consumer issue

--- backend/api/ulca-error-consumer/service/cronerror.py
+++ backend/api/ulca-error-consumer/service/cronerror.py
@@ -58,7 +58,8 @@ class ErrorProcessor(Thread):
                             { "$group": { "_id" : None, "consolidatedCount" : { "$sum": "$count" } } },
                             {"$project":{ "_id":0,"consolidatedCount":1}}]
                 present_count = errorepo.aggregate(agg_query)
-
+                if len(present_count) == 0:
+                    continue
                 check_query   = {"serviceRequestNumber" : srn,"uploaded" : True} 
                 consolidated_rec = errorepo.search(check_query, {"_id":False}, None, None) #  Respone - Null --> Summary report havent't generated yet
                 if  not consolidated_rec:
