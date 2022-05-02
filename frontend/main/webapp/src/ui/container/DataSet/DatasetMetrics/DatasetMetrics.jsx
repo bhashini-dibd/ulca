102e985a5c08e4fd65d90a5409aa22586cd86f73
AlpanaMajhi <alpanamajhi001@gmail.com>
2022-05-02 11:22:30 +0530

    Columms added

--- frontend/main/webapp/src/ui/container/DataSet/DatasetMetrics/DatasetMetrics.jsx
+++ frontend/main/webapp/src/ui/container/DataSet/DatasetMetrics/DatasetMetrics.jsx
@@ -7,7 +7,15 @@ const DatasetMetrics = () => {
     selectableRows: false,
     filter: false,
   };
-  const columns = [{ name: "datasetType", label: "Dataset Type" }];
+  const columns = [
+    { name: "datasetType", label: "Dataset Type" },
+    { name: "sourceLanguage", label: "Source Language" },
+    { name: "targetLanguage", label: "Target Language" },
+    { name: "domain", label: "Domain" },
+    { name: "collectionMethod", label: "Collection Method" },
+    { name: "submitter", label: "Submitter" },
+    { name: "count", label: "Count" },
+  ];
 
   return (
     <DataTable
