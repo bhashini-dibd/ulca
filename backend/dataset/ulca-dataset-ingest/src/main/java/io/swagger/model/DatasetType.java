38864ce608a861b2e9de4d9ab2b1423aa3dbcfa2
sarojsingh2021 <84913529+sarojsingh2021@users.noreply.github.com>
2022-05-18 17:38:54 +0530

    transliteration dataset changes
    transliteration dataset changes

--- backend/dataset/ulca-dataset-ingest/src/main/java/io/swagger/model/DatasetType.java
+++ backend/dataset/ulca-dataset-ingest/src/main/java/io/swagger/model/DatasetType.java
@@ -15,6 +15,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 public enum DatasetType {
   PARALLEL_CORPUS("parallel-corpus"),
     MONOLINGUAL_CORPUS("monolingual-corpus"),
+    TRANSLITERATION_CORPUS("transliteration-corpus"),
     ASR_CORPUS("asr-corpus"),
     TTS_CORPUS("tts-corpus"),
     ASR_UNLABELED_CORPUS("asr-unlabeled-corpus"),
