d679d4c2435b237c3289ccc454f9eb1e800eeb0c
Siddanthshaiva <siddanth.shaiva@gmail.com>
2022-05-25 21:01:01 +0530

    add transliterationSrcLang

--- backend/api/master-data-management-service/mdms/utilities/mdms_utils.py
+++ backend/api/master-data-management-service/mdms/utilities/mdms_utils.py
@@ -30,7 +30,7 @@ class MdUtils:
         log.info(f'path expression {path_expression}')
         log.info(f'json_data{json_data}')
         values          =   path_expression.find(json_data)[0].value
-
+        log.info(f'values {values}')
         return values
 
 
