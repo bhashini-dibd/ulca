6e782f466a7ab673edb8037c56e5d13b7dd61f19
Siddanthshaiva <siddanth.shaiva@gmail.com>
2022-06-02 13:26:45 +0530

    add accuracy metrics for transliteration

--- backend/model/benchmark-metrics/metrics/transliteration_cer_eval.py
+++ backend/model/benchmark-metrics/metrics/transliteration_cer_eval.py
@@ -15,32 +15,46 @@ class TransliterationCEREval(ModelMetricEval):
     #def __init__(self):
     #    self.cer_score = load_metric('cer', revision="master")
 
+    def levenshtein(self,u, v):
+        prev = None
+        curr = [0] + list(range(1, len(v) + 1))
+        # Operations: (SUB, DEL, INS)
+        prev_ops = None
+        curr_ops = [(0, 0, i) for i in range(len(v) + 1)]
+        for x in range(1, len(u) + 1):
+            prev, curr = curr, [x] + ([None] * len(v))
+            prev_ops, curr_ops = curr_ops, [(0, x, 0)] + ([None] * len(v))
+            for y in range(1, len(v) + 1):
+                delcost = prev[y] + 1
+                addcost = curr[y - 1] + 1
+                subcost = prev[y - 1] + int(u[x - 1] != v[y - 1])
+                curr[y] = min(subcost, delcost, addcost)
+                if curr[y] == subcost:
+                    (n_s, n_d, n_i) = prev_ops[y - 1]
+                    curr_ops[y] = (n_s + int(u[x - 1] != v[y - 1]), n_d, n_i)
+                elif curr[y] == delcost:
+                    (n_s, n_d, n_i) = prev_ops[y]
+                    curr_ops[y] = (n_s, n_d + 1, n_i)
+                else:
+                    (n_s, n_d, n_i) = curr_ops[y - 1]
+                    curr_ops[y] = (n_s, n_d, n_i + 1)
+        return curr[len(v)], curr_ops[len(v)]
+
+
     def transliteration_metric_eval(self, ground_truth, machine_translation):
 
         try:
-            prev = None
-            curr = [0] + list(range(1, len(machine_translation) + 1))
-            # Operations: (SUB, DEL, INS)
-            prev_ops = None
-            curr_ops = [(0, 0, i) for i in range(len(machine_translation) + 1)]
-            for x in range(1, len(ground_truth) + 1):
-                prev, curr = curr, [x] + ([None] * len(machine_translation))
-                prev_ops, curr_ops = curr_ops, [(0, x, 0)] + ([None] * len(machine_translation))
-                for y in range(1, len(machine_translation) + 1):
-                    delcost = prev[y] + 1
-                    addcost = curr[y - 1] + 1
-                    subcost = prev[y - 1] + int(ground_truth[x - 1] != machine_translation[y - 1])
-                    curr[y] = min(subcost, delcost, addcost)
-                    if curr[y] == subcost:
-                        (n_s, n_d, n_i) = prev_ops[y - 1]
-                        curr_ops[y] = (n_s + int(ground_truth[x - 1] != machine_translation[y - 1]), n_d, n_i)
-                    elif curr[y] == delcost:
-                        (n_s, n_d, n_i) = prev_ops[y]
-                        curr_ops[y] = (n_s, n_d + 1, n_i)
-                    else:
-                        (n_s, n_d, n_i) = curr_ops[y - 1]
-                        curr_ops[y] = (n_s, n_d, n_i + 1)
-            return curr[len(machine_translation)], curr_ops[len(machine_translation)]
+            
+            _, (s, i, d) = self.levenshtein(self, ground_truth, machine_translation)
+            cer_s = s
+            cer_i = i
+            cer_d = d
+            cer_n = len(ground_truth)
+
+            if cer_n > 0:
+                return (cer_s + cer_i + cer_d) / cer_n
+            else:
+                return 0
 
             #if ground_truth and machine_translation:
             #    eval_score = self.cer_score.compute(predictions=machine_translation, references=ground_truth)
