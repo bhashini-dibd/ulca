da989ff2393cdb55a4f3fabccb8ecf9fce794a13
Aswin Pradeep <aswinpradeep1@gmail.com>
2022-07-11 00:08:01 +0530

    Adding validation pipeline for glossary dataset

--- backend/dataset/validate/kafkawrapper/consumer.py
+++ backend/dataset/validate/kafkawrapper/consumer.py
@@ -11,9 +11,11 @@ from service.monolingual import MonolingualValidate
 from service.asr_unlabeled import ASRUnlabeledValidate
 from service.tts import TTSValidate
 from service.transliteration import TransliterationValidate
+from service.glossary import GlossaryValidate
+
 
 from configs.configs import kafka_bootstrap_server_host, validate_input_topic, validate_consumer_grp, validate_output_topic, user_mode_real
-from configs.configs import dataset_type_parallel, dataset_type_asr, dataset_type_ocr, dataset_type_monolingual, dataset_type_asr_unlabeled, dataset_type_tts, dataset_type_transliteration
+from configs.configs import dataset_type_parallel, dataset_type_asr, dataset_type_ocr, dataset_type_monolingual, dataset_type_asr_unlabeled, dataset_type_tts, dataset_type_transliteration, dataset_type_glossary
 from kafka import KafkaConsumer
 from processtracker.processtracker import ProcessTracker
 from kafkawrapper.producer import Producer
@@ -38,7 +40,7 @@ def consume():
     try:
         topics = [validate_input_topic]
         consumer = instantiate(topics)
-        p_service, o_service, a_service, m_service, au_service, t_service, tl_service = ParallelValidate(), OCRValidate(), ASRValidate(), MonolingualValidate(), ASRUnlabeledValidate(), TTSValidate(), TransliterationValidate()
+        p_service, o_service, a_service, m_service, au_service, t_service, tl_service, g_service = ParallelValidate(), OCRValidate(), ASRValidate(), MonolingualValidate(), ASRUnlabeledValidate(), TTSValidate(), TransliterationValidate(), GlossaryValidate()
         pt = ProcessTracker()
         prod = Producer()
         rand_str = ''.join(random.choice(string.ascii_letters) for i in range(4))
@@ -71,6 +73,8 @@ def consume():
                             t_service.execute_validation_pipeline(data)
                         if data["datasetType"] == dataset_type_transliteration:
                             tl_service.execute_validation_pipeline(data)
+                        if data["datasetType"] == dataset_type_glossary:
+                            g_service.execute_validation_pipeline(data)
                     else:
                         break
                 except Exception as e:
