358ffcbed88d8902ca1d76bb0e4eb9d9917e08a2
Aswin Pradeep <aswinpradeep1@gmail.com>
2022-11-23 21:03:03 +0530

    assertLanguage  search support

--- backend/dataset/publish/service/asrunlabeled.py
+++ backend/dataset/publish/service/asrunlabeled.py
@@ -207,6 +207,10 @@ class ASRUnlabeledService:
             db_query, tags = {}, []
             if 'sourceLanguage' in query.keys():
                 db_query["sourceLanguage"] = {"$in": query["sourceLanguage"]}
+            if 'mixedDataSource' in query.keys():
+                db_query["mixedDataSource"] = query["mixedDataSource"]
+            if 'assertLanguage' in query.keys():
+                db_query["assertLanguage"] = {"$in": query["assertLanguage"]}
             if 'collectionMethod' in query.keys():
                 tags.extend(query["collectionMethod"])
             if 'license' in query.keys():
