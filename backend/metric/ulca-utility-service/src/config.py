8b88013266854904da53394ac3a73952d77c728b
Siddanthshaiva <siddanth.shaiva@gmail.com>
2022-11-28 00:01:18 +0530

    cron time

--- backend/metric/ulca-utility-service/src/config.py
+++ backend/metric/ulca-utility-service/src/config.py
@@ -53,7 +53,7 @@ process_db_schema           =   os.environ.get('PROCESS_DB','ulca-process-tracke
 process_col                 =   os.environ.get('PROCESS_COL','ulca-pt-processes')
 tasks_col                   =   os.environ.get('TASKS_COL','ulca-pt-tasks')
 
-metric_cron_interval_sec     =  60
+metric_cron_interval_sec     =  21600
 filter_cron_interval_sec     =   os.environ.get('FILTER_CRON_INTERVAL_SEC',300)#14400
 if isinstance(filter_cron_interval_sec, str):
     filter_cron_interval_sec =  eval(filter_cron_interval_sec)
