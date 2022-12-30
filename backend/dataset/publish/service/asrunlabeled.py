0a0cf9cfb23641b9a0f8c9baaf7a5f81c81389a8
rathan-muralidhar <rathan.muralidhar@gmail.com>
2022-12-30 10:19:51 +0530

    Refactored audio and image file names

--- backend/dataset/publish/service/asrunlabeled.py
+++ backend/dataset/publish/service/asrunlabeled.py
@@ -151,7 +151,7 @@ class ASRUnlabeledService:
                 #insert_data["objStorePath"] = "Something"
                 #insert_data["refImgStorePath"] = "Else"
                 epoch = eval(str(time.time()).replace('.', '')[0:13])
-                data['audioFilename'] = data['audioFilename'].split('/')[-1]
+                data['audioFilename'] = data['audioFilename']#.split('/')[-1]
                 s3_file_name = f'{metadata["datasetId"]}|{epoch}|{data["audioFilename"]}'
                 object_store_path = utils.upload_file(data["fileLocation"], asr_unlabeled_prefix, s3_file_name)
                 if not object_store_path:
@@ -162,7 +162,7 @@ class ASRUnlabeledService:
                     epoch = eval(str(time.time()).replace('.', '')[0:13])
                     if isinstance(data['imageFilename'],list):
                         data['imageFilename'] = data['imageFilename'][0]
-                    imageFileName = data['imageFilename'].split('/')[-1]
+                    imageFileName = data['imageFilename']#.split('/')[-1]
                     s3_img_file_name = f'{metadata["datasetId"]}|{epoch}|{imageFileName}'
                     img_object_store_path = utils.upload_file(data["imageFileLocation"], asr_unlabeled_prefix, s3_img_file_name)
                     # log.info(f"Test57 {img_object_store_path}")
