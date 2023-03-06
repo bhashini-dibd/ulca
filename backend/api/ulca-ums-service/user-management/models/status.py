cc198aaf4cee0ceb10d4173344cdea4cb613dfc0
rathan-muralidhar <rathan.muralidhar@gmail.com>
2023-03-06 21:16:26 +0530

    Reverting to previous stage of ums

--- backend/api/ulca-ums-service/user-management/models/status.py
+++ backend/api/ulca-ums-service/user-management/models/status.py
@@ -19,7 +19,6 @@ class Status(enum.Enum):
     SUCCESS_GENERATE_APIKEY =   {"message" : "UserApiKey generated successfully"}
     SUCCESS_FOUND_APIKEY    =   {"message" : "ApiKey found successfully"}
     SUCCESS_USER_APIKEY     =   {"message" : "UserApiKey found and revoked successfully"}
-    SUCCESS_GENERATE_APIKEY =  {"message" : "UserApiKey generated successfully"}
 
 
    
