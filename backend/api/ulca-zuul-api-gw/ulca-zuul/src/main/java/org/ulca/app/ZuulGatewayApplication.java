ef218b2476d597e1d27268cb867f57cbeaed00b0
rathan-muralidhar <rathan.muralidhar@gmail.com>
2023-03-13 21:10:51 +0530

    Added CORS in Zuul

--- backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/app/ZuulGatewayApplication.java
+++ backend/api/ulca-zuul-api-gw/ulca-zuul/src/main/java/org/ulca/app/ZuulGatewayApplication.java
@@ -24,6 +24,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @EnableZuulProxy
@@ -47,7 +50,24 @@ public class ZuulGatewayApplication {
 
     @Value(value = "${redis.ratelimit.db}")
     private Integer ratelimitDb;
-
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        //config.addAllowedMethod("OPTIONS");
+        //config.addAllowedMethod("HEAD");
+        //config.addAllowedMethod("GET");
+        //config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        //config.addAllowedMethod("DELETE");
+        //config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
     @Bean
     public JedisConnectionFactory connectionFactory() {
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
