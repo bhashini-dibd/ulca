38cb1954380efe9789f3552fccd34feef09af7d8
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2023-07-11 16:36:12 +0530

    .

--- frontend/intro/webapp/src/components/ModelChart.jsx
+++ frontend/intro/webapp/src/components/ModelChart.jsx
@@ -23,7 +23,7 @@ const ChartRender = (props) => {
 	const [filterValue, setFilterValue] = useState("language");
   const [data, setData] = useState([]);
   const [dataValue, setDataValue] = useState("");
-  const [startIndex, setStartIndex] = useState(20);
+  const [endIndex, setEndIndex] = useState(20);
 
 	const [page, setPage] = useState(0);
 	const [sourceLanguage, setSourceLanguage] = useState(
@@ -235,7 +235,7 @@ const ChartRender = (props) => {
 								</XAxis>
 								<YAxis padding={{ top: 80 }} tickInterval={10} allowDecimals={false} type="number" dx={0} tickFormatter={(value) => new Intl.NumberFormat('en', { notation: "compact" }).format(value)}><Label value= {axisValue.yAxis} angle= {-90} position= 'insideLeft' fontWeight="bold" fontSize={16}></Label></YAxis>
 
-								{data.length > 100 && <Brush dataKey='label' height={30} stroke="#8884d8" endIndex={startIndex}/> }
+								{data.length > 100 && <Brush dataKey='label' height={30} stroke="#8884d8" endIndex={endIndex}/> }
 								<Tooltip contentStyle={{fontFamily:"Roboto", fontSize:"14px"}} formatter={(value) => new Intl.NumberFormat('en').format(value)} cursor={{ fill: 'none' }} />
 								<Bar margin={{ top: 140, left: 20, right: 20, bottom: 20 }} dataKey="value" cursor="pointer" radius={[8, 8, 0, 0]} maxBarSize={65} onClick={(event) => { handleOnClick(page + 1, event) }}>
 									<LabelList
