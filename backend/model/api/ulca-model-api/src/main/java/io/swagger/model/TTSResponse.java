867ceebcfa152ee43ca644c9a600b380546cbccb
insphereayush <insphere.ayush@gmail.com>
2023-12-06 16:21:30 +0530

    TTSResponse class modified and Detail removed

--- backend/model/api/ulca-model-api/src/main/java/io/swagger/model/TTSResponse.java
+++ backend/model/api/ulca-model-api/src/main/java/io/swagger/model/TTSResponse.java
@@ -1,155 +1,136 @@
 package io.swagger.model;
 
-import java.util.List;
 import java.util.Objects;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.model.AudioFiles;
+import io.swagger.model.SupportedTasks;
+import io.swagger.model.TTSResponseConfig;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.validation.annotation.Validated;
-
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 /**
- * the response for translation. Standard http status codes to be used.
+ * the response for translation.  Standard http status codes to be used.
  */
-@Schema(description = "the response for translation. Standard http status codes to be used.")
+@Schema(description = "the response for translation.  Standard http status codes to be used.")
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2023-04-10T05:17:35.492966412Z[GMT]")
-public class TTSResponse {
-    @JsonProperty("audio")
-    private AudioFiles audio = null;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TTSResponse   {
+  @JsonProperty("audio")
+  private AudioFiles audio = null;
 
-    @JsonProperty("config")
-    private TTSResponseConfig config = null;
+  @JsonProperty("config")
+  private TTSResponseConfig config = null;
 
-    @JsonProperty("taskType")
-    private SupportedTasks taskType = null;
+  @JsonProperty("taskType")
+  private SupportedTasks taskType = null;
 
-    @JsonProperty("detail")
-    private List<Detail> detail = null;
+  public TTSResponse audio(AudioFiles audio) {
+    this.audio = audio;
+    return this;
+  }
 
-    public TTSResponse audio(AudioFiles audio) {
-        this.audio = audio;
-        return this;
-    }
+  /**
+   * Get audio
+   * @return audio
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
 
-    /**
-     * Get audio
-     *
-     * @return audio
-     **/
-    @Schema(required = true, description = "")
-    @NotNull
     @Valid
     public AudioFiles getAudio() {
-        return audio;
-    }
+    return audio;
+  }
 
-    public void setAudio(AudioFiles audio) {
-        this.audio = audio;
-    }
+  public void setAudio(AudioFiles audio) {
+    this.audio = audio;
+  }
 
-    public TTSResponse config(TTSResponseConfig config) {
-        this.config = config;
-        return this;
-    }
+  public TTSResponse config(TTSResponseConfig config) {
+    this.config = config;
+    return this;
+  }
 
-    /**
-     * Get config
-     *
-     * @return config
-     **/
-    @Schema(description = "")
+  /**
+   * Get config
+   * @return config
+   **/
+  @Schema(description = "")
+  
     @Valid
     public TTSResponseConfig getConfig() {
-        return config;
-    }
+    return config;
+  }
 
-    public void setConfig(TTSResponseConfig config) {
-        this.config = config;
-    }
+  public void setConfig(TTSResponseConfig config) {
+    this.config = config;
+  }
 
-    public TTSResponse taskType(SupportedTasks taskType) {
-        this.taskType = taskType;
-        return this;
-    }
+  public TTSResponse taskType(SupportedTasks taskType) {
+    this.taskType = taskType;
+    return this;
+  }
 
-    /**
-     * Get taskType
-     *
-     * @return taskType
-     **/
-    @Schema(description = "")
+  /**
+   * Get taskType
+   * @return taskType
+   **/
+  @Schema(description = "")
+  
     @Valid
     public SupportedTasks getTaskType() {
-        return taskType;
-    }
+    return taskType;
+  }
 
-    public void setTaskType(SupportedTasks taskType) {
-        this.taskType = taskType;
-    }
+  public void setTaskType(SupportedTasks taskType) {
+    this.taskType = taskType;
+  }
 
-    public TTSResponse detail(List<Detail> detail) {
-        this.detail = detail;
-        return this;
-    }
 
-    /**
-     * Get detail
-     *
-     * @return detail
-     **/
-    @Schema(description = "")
-    @Valid
-    public List<Detail> getDetail() {
-        return detail;
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public void setDetail(List<Detail> detail) {
-        this.detail = detail;
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
+    TTSResponse ttSResponse = (TTSResponse) o;
+    return Objects.equals(this.audio, ttSResponse.audio) &&
+        Objects.equals(this.config, ttSResponse.config) &&
+        Objects.equals(this.taskType, ttSResponse.taskType);
+  }
 
-    @Override
-    public boolean equals(java.lang.Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        TTSResponse ttSResponse = (TTSResponse) o;
-        return Objects.equals(this.audio, ttSResponse.audio) &&
-                Objects.equals(this.config, ttSResponse.config) &&
-                Objects.equals(this.taskType, ttSResponse.taskType) &&
-                Objects.equals(this.detail, ttSResponse.detail);
+  @Override
+  public int hashCode() {
+    return Objects.hash(audio, config, taskType);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class TTSResponse {\n");
+    
+    sb.append("    audio: ").append(toIndentedString(audio)).append("\n");
+    sb.append("    config: ").append(toIndentedString(config)).append("\n");
+    sb.append("    taskType: ").append(toIndentedString(taskType)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(audio, config, taskType, detail);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class TTSResponse {\n");
-
-        sb.append("    audio: ").append(toIndentedString(audio)).append("\n");
-        sb.append("    config: ").append(toIndentedString(config)).append("\n");
-        sb.append("    taskType: ").append(toIndentedString(taskType)).append("\n");
-        sb.append("    detail: ").append(toIndentedString(detail)).append("\n");
-        sb.append("}");
-        return sb.toString();
-    }
-
-    /**
-     * Convert the given object to string with each line indented by 4 spaces (except the first line).
-     */
-    private String toIndentedString(java.lang.Object o) {
-        if (o == null) {
-            return "null";
-        }
-        return o.toString().replace("\n", "\n    ");
-    }
+    return o.toString().replace("\n", "\n    ");
+  }
 }
