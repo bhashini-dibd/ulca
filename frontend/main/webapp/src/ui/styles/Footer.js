58a91ad48787e45eaaa0eb92a6d4343aecf205ed
aamirpashaaa <aamir@zerozilla.com>
2024-01-16 17:34:58 +0530

    changes

--- frontend/main/webapp/src/ui/styles/Footer.js
+++ frontend/main/webapp/src/ui/styles/Footer.js
@@ -1,18 +1,109 @@
 const FooterStyle = (theme) => ({
-    
-    grid: {display:"flex",background:"#DFEAFB",padding:"30px 50px", color:"#16337B",alignItems:"center"},
-    grid2: {display:"flex",background:"#C6D9F4",padding:"30px 50px", color:"#16337B",alignItems:"center"},
-    grid3: {display:"flex",background:"#B3CAED",padding:"30px 50px", color:"#16337B",alignItems:"center"},
-    social :{display:"flex",marginLeft:"auto",width:"40%",justifyContent:"space-between","@media  (max-device-width : 650px)": { marginTop:"20px"}},
-    container:{width:"100%",display:"flex",boxSizing:"border-box",flexWrap:"wrap" },
-    bhasini:{display:"flex",marginRight:"auto",width:"80%",justifyContent:"space-between",marginTop:"10px", "@media  (max-device-width : 650px)": { width:"100%"},},
-    info :{display:"flex",marginLeft:"auto",width:"80%",justifyContent:"space-between",marginTop:"30px"},
-    copy:{display:"flex",marginRight:"auto",width:"80%",justifyContent:"space-between",marginTop:"30px"},
-    parent:{background:"#0F2749 !important"},
-    parentDiv:{maxWidth:"1140px",display:"flex",margin:"auto"},
-    textAlign:{textAlign:"center",marginTop:"2rem"},
-    image:{marginRight:"1rem"},
-    link:{fontWeight:"bold", color:"#16337B",marginTop:"10px",overflowWrap:"anywhere"}
-  });
-  export default FooterStyle;
+  grid: {
+    display: "flex",
+    background: "#DFEAFB",
+    padding: "30px 50px",
+    color: "#16337B",
+    alignItems: "center",
+  },
+  grid2: {
+    display: "flex",
+    background: "#C6D9F4",
+    padding: "30px 50px",
+    color: "#16337B",
+    alignItems: "center",
+  },
+  grid3: {
+    display: "flex",
+    background: "#B3CAED",
+    padding: "30px 50px",
+    color: "#16337B",
+    alignItems: "center",
+  },
+  social: {
+    display: "flex",
+    marginLeft: "auto",
+    width: "40%",
+    justifyContent: "space-between",
+    "@media  (max-device-width : 650px)": {
+      marginTop: "20px",
+      width: "95%",
+      justifyContent: "space-evenly",
+    },
+  },
+  container: {
+    width: "100%",
+    display: "flex",
+    boxSizing: "border-box",
+    flexWrap: "wrap",
+  },
+  bhasini: {
+    display: "flex",
+    marginRight: "auto",
+    width: "80%",
+    justifyContent: "space-between",
+    marginTop: "10px",
+    "@media  (max-device-width : 650px)": {
+      width: "100%",
+      justifyContent: "space-evenly",
+      margin: "0 auto",
+    },
+  },
+  info: {
+    display: "flex",
+    marginLeft: "auto",
+    width: "80%",
+    justifyContent: "space-between",
+    marginTop: "30px",
+    "@media  (max-device-width : 650px)": {
+      width: "100%",
+      justifyContent: "space-evenly",
+      margin: "0 auto",
+    },
+  },
+  infoNew: {
+    display: "flex",
+    marginLeft: "auto",
+    width: "80%",
+    justifyContent: "space-between",
+    marginTop: "20px",
+    "@media  (max-device-width : 650px)": {
+      width: "100%",
+      justifyContent: "space-evenly",
+      margin: "0 auto",
+    },
+  },
+  copy: {
+    display: "flex",
+    marginRight: "auto",
+    width: "80%",
+    marginTop: "30px",
+  },
+  parent: { background: "#0F2749 !important" },
+  parentDiv: { maxWidth: "1240px", display: "flex", margin: "auto" },
+  textAlign: { textAlign: "center", marginTop: "2rem" },
+  image: { marginRight: "1rem" },
+  link: {
+    // fontWeight: "bold",
+    color: "black",
+    marginTop: "10px",
+    overflowWrap: "anywhere",
+  },
+  tdlLogo: { "@media  (max-device-width : 650px)": { width: "100%", marginTop:"20px" } },
+  FooterLogo: {
+    display: "flex",
+    marginTop: "2rem",
+    justifyContent: "flex-end",
+    marginBottom: "1rem",
+    "@media  (max-device-width : 650px)": { width: "100%", justifyContent:"center" },
+  },
+  BottomText: {
+    display: "flex",
+    marginLeft:"3rem",
+    justifyContent: "flex-start",
   
+    marginBottom: "2rem",
+    "@media  (max-device-width : 650px)": { width: "90%", justifyContent:"center" },
+  },
+});
+export default FooterStyle;
