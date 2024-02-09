cadde25df829f77f158a6205a82adfbd161268f8
aamirpashaaa <aamir@zerozilla.com>
2024-02-09 12:59:12 +0530

    frontend changes

--- frontend/intro/webapp/src/Layout.js
+++ frontend/intro/webapp/src/Layout.js
@@ -2,15 +2,22 @@ import React, { useState } from "react";
 import Footer from "./components/Footer";
 import { WhyULCA } from "./components/WhyULCA";
 import FAQ from "./components/FAQ";
+import HomeBanner from "./components/HomeBanner";
+import HomeDatasets from "./components/HomeDatasets";
+import VideoSection from "./components/VideoSection";
+import Header from "./components/Header";
 
 function App(props) {
   return (
     <>
-    <WhyULCA /> 
-    <FAQ />
-    <Footer />
+    <Header />
+      <HomeBanner />
+      <HomeDatasets />
+      <VideoSection />
+      <WhyULCA />
+      {/* <FAQ /> */}
+      <Footer />
     </>
-    
   );
 }
 export default App;
