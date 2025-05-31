e213a7a98b21f016490fc1e88ac922525e4b0c7c
rathan-muralidhar <rathan.muralidhar@gmail.com>
2025-05-31 17:31:36 +0530

    Update logic for find_one in record matching

--- backend/api/ulca-ums-service/user-management/models/user_auth.py
+++ backend/api/ulca-ums-service/user-management/models/user_auth.py
@@ -180,20 +180,20 @@ class UserAuthenticationModel(object):
             #fetching user record matching userName and userID
             record = collections.find_one({"email": user_email,"userID":user_id})
             if record:
-                for user in record:
-                    register_time = user["registeredTime"]
-                    name  = user["firstName"]
-                    #checking whether verfication link had expired or not
-                    if (datetime.utcnow() - register_time) > timedelta(hours=verify_mail_expiry):
-                        log.exception("Verification link expired for {}".format(user_email))
-                        #Removing previous record from db
-                        collections.delete_many({"email": user_email,"userID":user_id})
-                        return post_error("Data Not valid","Verification link expired. Please sign up again.",None)
+                user = record
+                register_time = user["registeredTime"]
+                name  = user["firstName"]
+                #checking whether verfication link had expired or not
+                if (datetime.utcnow() - register_time) > timedelta(hours=verify_mail_expiry):
+                    log.exception("Verification link expired for {}".format(user_email))
+                    #Removing previous record from db
+                    collections.delete_many({"email": user_email,"userID":user_id})
+                    return post_error("Data Not valid","Verification link expired. Please sign up again.",None)
 
-                    results = collections.update(user, {"$set": {"isVerified": True,"isActive": True,"activatedTime": datetime.utcnow()}})
-                    if 'writeError' in list(results.keys()):
-                            return post_error("Database error", "writeError whie updating record", None)
-                    log.info("Record updated for {}, activation & verification statuses are set to True".format(user_email))
+                results = collections.update(user, {"$set": {"isVerified": True,"isActive": True,"activatedTime": datetime.utcnow()}})
+                if 'writeError' in list(results.keys()):
+                        return post_error("Database error", "writeError whie updating record", None)
+                log.info("Record updated for {}, activation & verification statuses are set to True".format(user_email))
             else:
                 log.exception("No proper database records found for activation of {}".format(user_email))
                 return post_error("Data Not valid","No records matching the given parameters ",None) 
