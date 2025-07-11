a9126e53f256b3585e1783a6f0652e155247b3c3
parshanttarento <parshant.tyagi@tarento.com>
2025-07-11 11:35:09 +0530

    chrf-change

--- backend/model/benchmark-metrics/metrics/chrf/chrf.py
+++ backend/model/benchmark-metrics/metrics/chrf/chrf.py
@@ -2,7 +2,28 @@ import datasets
 import numpy as np
 from nltk.translate import chrf_score
 
+_DESCRIPTION = """\
+CHRF is an automatic evaluation metric for machine translation based on character n-gram precision and recall.
+"""
 
+_CITATION = """\
+@inproceedings{popovic-2015-chrf,
+    title = "{CHRF}: character n-gram {F}-score for automatic {MT} evaluation",
+    author = "Popovic, Maja",
+    booktitle = "Proceedings of the Tenth Workshop on Statistical Machine Translation",
+    year = "2015",
+    pages = "392--395",
+}
+"""
+
+_KWARGS_DESCRIPTION = """
+Args:
+    predictions: list of predicted sentences
+    references: list of reference sentences
+
+Returns:
+    chrf: average CHRF score
+"""
 
 class CHRF(datasets.Metric):
     def _info(self):
